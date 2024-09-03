cf0d22b77c962dea6cb15f63e2820ef83c46c9ff
pszczesz <pszczesz@gmail.com>
2024-09-03 12:51:01 +0200

    ff

--- 4pa_gr2/cw1/obj/Debug/net8.0/cw1.AssemblyInfo.cs
+++ 4pa_gr2/cw1/obj/Debug/net8.0/cw1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cw1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+727c899595800877058867cc48c8b84cfeea7b1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679fa3b492eeed587231731705ecf0e41aa1d259")]
 [assembly: System.Reflection.AssemblyProductAttribute("cw1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cw1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
