04982547149e5f70ea2c5e5f489bbdf792c91bb8
tiszczesz <tiszczesz@users.noreply.github.com>
2024-09-09 14:32:28 +0200

    jj

---
+++ 4pb_gr1/cw1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Kolejowe(){
+            return View();
+        }
+
+    }
+}
--- 4pb_gr1/cw1/Program.cs
+++ 4pb_gr1/cw1/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseStaticFiles();
+// app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 app.Run();
