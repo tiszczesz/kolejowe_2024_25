82a5297ad51307c8a5c63190578667a8bcee9a10
tiszczesz <tiszczesz@users.noreply.github.com>
2024-09-09 18:13:09 +0200

    gg

--- 4pb_gr2/cw1/Models/ProductsRepo.cs
+++ 4pb_gr2/cw1/Models/ProductsRepo.cs
@@ -1,5 +1,5 @@
 using System;
-
+using Microsoft.Data.Sqlite;
 namespace cw1.Models;
 
 public class ProductsRepo
@@ -11,7 +11,20 @@ public class ProductsRepo
     }
     public List<Product> GetProducts(){
         var products = new List<Product>();
-
+        using var conn = new SqliteConnection(_connString);
+        SqliteCommand cmd = conn.CreateCommand();
+        cmd.CommandText = "SELECT * FROM Products";
+        conn.Open();
+        SqliteDataReader reader = cmd.ExecuteReader();
+        while (reader.Read()){
+            products.Add(new Product
+            {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Category = reader.GetString(2),
+                Price = reader.GetDecimal(3)
+            });
+        }
         return products;
     }
 }
