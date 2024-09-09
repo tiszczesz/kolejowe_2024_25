a13b43f1a8e86b0b495d6e21f71b547702f9e79c
tiszczesz <tiszczesz@users.noreply.github.com>
2024-09-09 15:37:26 +0200

    hh

--- 4pb_gr1/cw1/Controllers/HomeController.cs
+++ 4pb_gr1/cw1/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
+using cw1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw1.Controllers
 {
     public class HomeController : Controller
     {
+        private MoviesRepo _repo;
+        public HomeController(IConfiguration configuration)
+        {
+            _repo = new MoviesRepo(configuration);
+        }
         // GET: HomeController
         public ActionResult Index()
         {
-            return View();
+            var movies = _repo.GetMovies();
+            return View(movies);
         }
         public ActionResult Kolejowe(){
             return View();
