f0fca6cf29dec5fb6e8965b6d85fb12ccc084d33
tiszczesz <tiszczesz@users.noreply.github.com>
2024-09-16 15:41:22 +0200

    hh

--- 4pb_gr1/cw1/Models/Movie.cs
+++ 4pb_gr1/cw1/Models/Movie.cs
@@ -1,11 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace cw1.Models;
 
 public class Movie  //DTO
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+    
+    [Required(ErrorMessage = "Musisz podać tytuł")]   
+    
+    public string? Title { get; set; } 
+    
+    [Required(ErrorMessage = "Musisz podać reżysera")]
     public string? Director { get; set; }
-    public int Year { get; set; }
+    
+    [Range(1900, 2021, ErrorMessage = "Rok musi być z zakresu 1900-2021")]
+    [Required(ErrorMessage = "Musisz podać rok produkcji")]
+    public int? Year { get; set; }
 }
