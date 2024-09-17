70f55e71970fdc25b6b3bef7719c0a8402050d75
pszczesz <tiszczesz@users.noreply.github.com>
2024-09-17 15:25:00 +0200

    hh

--- 3pr_gr2/cw4/Program.cs
+++ 3pr_gr2/cw4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//definicja tablicy
+int[] tab = new int[10];
+int[] tab2 = new int[] { 1, 2, 3, 4, 5 };
+//wypełnienie tablicy
+for (int i = 0; i < tab.Length; i++)
+{
+    tab[i] = i * i;
+}
+//wyświetlenie tablicy
+ShowTable(tab);
+ShowTable(tab2);
+
+//utworzyc tablice 10 elementową i wypełnić ją napisami
+string[] napisy = new string[] { "Ala", "ma", "kota" };
+ShowTable(napisy);
+void ShowTable<T>(T[] tab)//metoda generyczna
+{
+    foreach (T element in tab)
+    {
+        Console.Write(element + " ");
+    }
+    Console.WriteLine();
+}
