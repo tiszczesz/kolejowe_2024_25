f045b9d763ff9730a11549f6211c02415cf76670
pszczesz <tiszczesz@users.noreply.github.com>
2024-09-17 15:01:05 +0200

    jj

--- 3pr_gr2/cw3/Person.cs
+++ 3pr_gr2/cw3/Person.cs
@@ -2,27 +2,37 @@ public class Person
 {
     private string firstname;
     private string lastname;
+    private int age;
 
-    public Person(string firstname, string lastname)
+    public Person(string firstname, string lastname, int age = 18) //konstruktor
     {
         this.firstname = firstname;
         this.lastname = lastname;
+        this.age = age;
     }
+
     public Person()
     {
         firstname = "";
         lastname = "";
+        age = 18;
     }
     public override string ToString()
     {
-        return Firstname + " " + lastname;
+        return Firstname + " " + Lastname + " wiek: " + Age;
+    }
+    //property dla pola age
+    public int Age
+    {
+        get { return age; }
+        set { age = value < 0 ? -value : value; }
     }
     public string Firstname
     {
         get { return firstname.ToUpper(); }
         set { firstname = value; }
     }
-        public string Lastname
+    public string Lastname
     {
         get { return lastname; }
         set { lastname = value; }
--- 3pr_gr2/cw3/Program.cs
+++ 3pr_gr2/cw3/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Person p1 = new Person();
-Person p2 = new Person("Jan", "Kowalski");
+Person p1 = new Person();//konstruktor domyślny
+Person p2 = new Person("Jan", "Kowalski");//konstruktor z parametrami
 p1.Firstname = "Adam";
 p1.Lastname = "Nowak";
+p1.Age = -10;
 
 Console.WriteLine(p1);
 Console.WriteLine(p2);
