b2b2f6280aecea77704549cca9cf7e32c129d7cb
tiszczesz <tiszczesz@users.noreply.github.com>
2024-09-23 15:18:52 +0200

    jj

--- temps/mvc-ef/Program.cs
+++ temps/mvc-ef/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using mvc_ef.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Add DbContext using SQLite
+builder.Services.AddDbContext<GameContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
