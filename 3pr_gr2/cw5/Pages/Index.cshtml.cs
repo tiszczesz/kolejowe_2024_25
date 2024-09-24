0c3fd06795a3732325b0633c4cda45b6ccbfbaad
pszczesz <tiszczesz@users.noreply.github.com>
2024-09-24 16:10:30 +0200

    hhh

---
+++ 3pr_gr2/cw5/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using cw5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cw5.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Person> Persons { get; set; }
+        public void OnGet()
+        {
+            Persons = GetPersons();
+        }
+        private List<Person> GetPersons()
+        {
+            var list = new List<Person>();
+            list.Add(new Person { FirstName = "Jan", LastName = "Kowalski", Age = 20 });
+            list.Add(new Person { FirstName = "Anna", LastName = "Nowak", Age = 30 });
+            list.Add(new Person { FirstName = "Andrzej", LastName = "Nowak", Age = 40 });
+            list.Add(new Person { FirstName = "Dominik", LastName = "Nowak", Age = 40 });
+            return list;
+        }
+    }
+}
