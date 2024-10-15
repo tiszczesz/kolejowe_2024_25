023ff180ebd32f2bcc57f3e78fad679fb0391788
pszczesz <tiszczesz@users.noreply.github.com>
2024-10-15 16:08:19 +0200

    hh

--- 3pr_gr2/cw6/Models/Movie.cs
+++ 3pr_gr2/cw6/Models/Movie.cs
@@ -1,13 +1,31 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace cw6.Models;
 
 public class Movie
 {
     public int Id { get; set; }
+    
+    [DisplayName("Tytuł")]
+    [Required(ErrorMessage = "Tytuł jest wymagany")]
     public string? Title { get; set; }
+    
+    [DisplayName("Reżyser")]
+    [Required(ErrorMessage = "Reżyser jest wymagany")]
     public string? Director { get; set; }
-    public DateOnly ReleaseDate { get; set; }
+    
+    [DisplayName("Data wydania")]
+    [Required(ErrorMessage = "Data wydania jest wymagana")]
+    public DateOnly? ReleaseDate { get; set; }
+    
+    [DisplayName("Gatunek")]
+    [Required(ErrorMessage = "Gatunek jest wymagany")]
     public string? Genre { get; set; }
+    
+    [DisplayName("Cena")]
+    [Required(ErrorMessage = "Cena jest wymagana")]
+    [Range(0, 2000, ErrorMessage = "Cena musi być większa od 0, 2000")]
     public double? Price { get; set; }
 }
