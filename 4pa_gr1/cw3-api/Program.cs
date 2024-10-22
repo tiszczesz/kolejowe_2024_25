b917336a2787054c1b81762e67464031e376ac3d
tiszczesz <tiszczesz@users.noreply.github.com>
2024-10-22 08:53:30 +0200

    kk

--- 4pa_gr1/cw3-api/Program.cs
+++ 4pa_gr1/cw3-api/Program.cs
@@ -2,24 +2,33 @@ using cw3_api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 //konstruktor wstrzykiwany bez argumentów
-builder.Services.AddScoped<IGamesRepo, SqliteGameRepo>();//wstrzykiwanie zależności
+//builder.Services.AddScoped<IGamesRepo, SqliteGameRepo>();//wstrzykiwanie zależności
 // Konfiguracja wstrzykiwania zależności z argumentem
 // builder.Services.AddScoped<IGamesRepo>(provider =>
 // {
 //     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //     return new GamesRepo(connectionString);
 // });
-
-
+//IGamesRepo repo = new FakeGamesRepo();//bez użycia wstrzykiwania zależności
+IGamesRepo repo = new SqliteGameRepo();//bez użycia wstrzykiwania zależności
 var app = builder.Build();
 
 
 
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/games", (IGamesRepo repo) => repo.GetGames());
-app.MapGet("/games/{id}", (IGamesRepo repo, int id) =>{
+app.MapGet("/games", () => repo.GetGames());
+app.MapGet("/games/{id}", ( int id) =>{
     var result =  repo.GetGameById(id);
     return result != null ? Results.Ok(result) : Results.NotFound();
     });
 app.Run();
+
+//gdy jest wstrzykiwanie zależności
+// app.MapGet("/", () => "Hello World!");
+// app.MapGet("/games", (IGamesRepo repo) => repo.GetGames());
+// app.MapGet("/games/{id}", (IGamesRepo repo, int id) =>{
+//     var result =  repo.GetGameById(id);
+//     return result != null ? Results.Ok(result) : Results.NotFound();
+//     });
+// app.Run();
