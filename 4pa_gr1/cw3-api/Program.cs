121db969723cdc9b1f9fa2147689d7f1e51712a3
tiszczesz <tiszczesz@users.noreply.github.com>
2024-10-22 09:44:26 +0200

    hh

--- 4pa_gr1/cw3-api/Program.cs
+++ 4pa_gr1/cw3-api/Program.cs
@@ -1,6 +1,16 @@
 using cw3_api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//konfiguracja CORS  dla przyszłego klienta np REACT na locahost
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyPolicy", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 //konstruktor wstrzykiwany bez argumentów
 //builder.Services.AddScoped<IGamesRepo, SqliteGameRepo>();//wstrzykiwanie zależności
 // Konfiguracja wstrzykiwania zależności z argumentem
@@ -9,29 +19,34 @@ var builder = WebApplication.CreateBuilder(args);
 //     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //     return new GamesRepo(connectionString);
 // });
+
 //IGamesRepo repo = new FakeGamesRepo();//bez użycia wstrzykiwania zależności
 IGamesRepo repo = new SqliteGameRepo();//bez użycia wstrzykiwania zależności
 var app = builder.Build();
 
-
+// uzycie CORS
+app.UseCors("MyPolicy");
 
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/games", () => repo.GetGames());
-app.MapGet("/games/{id}", ( int id) =>{
-    var result =  repo.GetGameById(id);
+app.MapGet("/games/{id}", (int id) =>
+{
+    var result = repo.GetGameById(id);
     return result != null ? Results.Ok(result) : Results.NotFound();
-    });
-app.MapDelete("/games/{id}", (int id) =>{
+});
+app.MapDelete("/games/{id}", (int id) =>
+{
     repo.DeleteGame(id);
     return Results.NoContent();
-    });
-app.MapPost("/games", (Game game) =>{
+});
+app.MapPost("/games", (Game game) =>
+{
     repo.AddGame(game);
     return Results.Created($"/games/{game.Id}", game);
-    });
+});
 
-    
+
 app.Run();
 
 //gdy jest wstrzykiwanie zależności
