18a9f6213dd1dd08f925d1718f24677fa98e7ee5
tiszczesz <tiszczesz@users.noreply.github.com>
2024-10-22 12:57:36 +0200

    kk

--- 4pa_gr2/cw4-api/Program.cs
+++ 4pa_gr2/cw4-api/Program.cs
@@ -1,7 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-var colors = new List<string> { "Red", "Green", "Blue" };
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/colors", () => colors);
+using cw4_api.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+//wstrzykiwanie zależności
+builder.Services.AddScoped<IGameRepo,FakeGamesRepo>();
+
+var app = builder.Build();
+// var colors = new List<string> { "Red", "Green", "Blue" };
+app.MapGet("/", () => "Hello World!");
+// app.MapGet("/colors", () => colors);
+app.MapGet("/games",(IGameRepo repo) =>repo.GetGames());
+app.MapGet("/games/{id}",((IGameRepo repo,int id)=>{
+    var game = repo.GetGameById(id);
+    return game!=null ? Results.Ok(game) : Results.NotFound();
+}));
 app.Run();
