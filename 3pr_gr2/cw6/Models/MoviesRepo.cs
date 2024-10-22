d5102431f566afbca2606006aee207e267accc24
tiszczesz <tiszczesz@users.noreply.github.com>
2024-10-22 15:44:12 +0200

    fff

--- 3pr_gr2/cw6/Models/MoviesRepo.cs
+++ 3pr_gr2/cw6/Models/MoviesRepo.cs
@@ -18,7 +18,27 @@ public class MoviesRepo
     {
         get { return _movies; }
     }
-
-
-
+    private void SaveChanges()
+    {
+         var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        string content = JsonSerializer.Serialize(_movies, options);
+        File.WriteAllText(_filePath, content);
+    }
+    private int GetNextId() //autoincrement
+    {
+        return _movies != null ? _movies.Max(m => m.Id) + 1 : 1;
+    }
+    public void AddMovie(Movie movie)
+    {
+        if (_movies == null)
+        {
+            _movies = new List<Movie>();
+        }
+        movie.Id = GetNextId();
+        _movies.Add(movie);
+        SaveChanges();
+    }
 }
--- 3pr_gr2/cw6/Pages/AddMovie.cshtml.cs
+++ 3pr_gr2/cw6/Pages/AddMovie.cshtml.cs
@@ -7,12 +7,41 @@ namespace cw6.Pages
     public class AddMovieModel : PageModel
     {
         [BindProperty]
-        public Movie MyMovie { get; set; }
+        public Movie? MyMovie { get; set; }
+        public List<string>? Genres { get; set; }
+        public AddMovieModel()
+        {
+            Genres = new List<string> {
+                 "Action", "Crime", "Drama",
+                 "Fantasy","Sci-Fi","Comedy",
+                 "Horror" };
+        }
         public void OnGet()
         {
+            ViewData["Genres"] = Genres;
+            ViewData["Message"] = "Dopiero wywitlamy formularz";
         }
-        public void OnPost(){
-
+        public IActionResult OnPost()
+        {
+            if(MyMovie == null)
+            {
+                ViewData["Message"] = "Brak danych";
+                return Page();
+            }
+            ViewData["Genres"] = Genres;
+            if (ModelState.IsValid)
+            {
+                // Add movie to database
+                MoviesRepo repo = new MoviesRepo("movies.json");
+                repo.AddMovie(MyMovie);
+                ViewData["Message"] = "Film dodany";
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ViewData["Message"] = "Niepoprawne dane";                
+            }
+            return Page();
         }
     }
 }
