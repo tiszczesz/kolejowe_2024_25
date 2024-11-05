da4c28abe43582f9c76d60f5c97b350cfc077221
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-05 13:23:36 +0100

    kk

--- 4pa_gr2/cw5-ef/Controllers/PersonController.cs
+++ 4pa_gr2/cw5-ef/Controllers/PersonController.cs
@@ -1,13 +1,21 @@
+using cw5_ef.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw5_ef.Controllers
 {
     public class PersonController : Controller
     {
+        private readonly PersonDbContext _context;
+        public PersonController(PersonDbContext context)
+        {
+            _context = context;
+        }
+      
         // GET: PersonController
         public ActionResult List()
         {
-            return View();
+            var people = _context.People.ToList();
+            return View(people);
         }
 
     }
