52de52cb52287b3dc65eb88756adad02155085e8
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-05 10:31:09 +0100

    kk

--- 4pa_gr1/cw4-ef/Controllers/ShopController.cs
+++ 4pa_gr1/cw4-ef/Controllers/ShopController.cs
@@ -1,13 +1,20 @@
+using cw4_ef.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw4_ef.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly ShopDbContext _context;
+        public ShopController(ShopDbContext context)
+        {
+            _context = context;
+        }
         // GET: ShopController
         public ActionResult List()
         {
-            return View();
+            var products = _context.Products.ToList();
+            return View(products);
         }
 
     }
