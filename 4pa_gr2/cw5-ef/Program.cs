2af9f53d630fda7aad954ae824489387b41a06a1
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-05 12:51:48 +0100

    mm

--- 4pa_gr2/cw5-ef/Program.cs
+++ 4pa_gr2/cw5-ef/Program.cs
@@ -1,7 +1,14 @@
+using cw5_ef.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 string? connString = builder.Configuration.GetConnectionString("Mysql");
 builder.Services.AddControllersWithViews();
+
 //dodanie Dbcontext
+builder.Services.AddDbContext<PersonDbContext>(
+        op => op.UseMySql(connString, ServerVersion.AutoDetect(connString))
+    );
 var app = builder.Build();
 app.UseStaticFiles();
 //app.MapGet("/", () => "Hello World!");
