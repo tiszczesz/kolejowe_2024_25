0acad38653d1bab4c15b0c2c9d51d267029321a4
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-05 14:20:04 +0100

    kk

--- 4pa_gr2/cw5-ef/Controllers/PersonController.cs
+++ 4pa_gr2/cw5-ef/Controllers/PersonController.cs
@@ -10,12 +10,41 @@ namespace cw5_ef.Controllers
         {
             _context = context;
         }
-      
+
         // GET: PersonController
         public ActionResult List()
         {
             var people = _context.People.ToList();
             return View(people);
+        }
+        [HttpGet]
+        public ActionResult AddPerson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddPerson(MyPerson person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.People.Add(person);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(person);
+        }
+        public IActionResult DeletePerson(int? id)
+        {
+            if (id != null)
+            {
+                var person = _context.People.Find(id);
+                if (person != null)
+                {
+                    _context.People.Remove(person);
+                    _context.SaveChanges();
+                }
+            }
+            return RedirectToAction("List");
         }
 
     }
--- 4pa_gr2/cw5-ef/Models/MyPerson.cs
+++ 4pa_gr2/cw5-ef/Models/MyPerson.cs
@@ -10,11 +10,16 @@ public class MyPerson
      public int Id { get; set; }
     
     [DisplayName("Imię")]
+    [Required(ErrorMessage = "Imię jest wymagane")]
+    [StringLength(50, ErrorMessage = "Imię nie może być dłuższe niż 50 znaków")]
     public string FirstName { get; set; }
     
     [DisplayName("Nazwisko")]
+    [Required(ErrorMessage = "Nazwisko jest wymagane")]
+    [StringLength(60, ErrorMessage = "Nazwisko nie może być dłuższe niż 60 znaków")]
     public string LastName { get; set; }
     
     [DisplayName("Data urodzenia")]
+    [Required(ErrorMessage = "Data urodzenia jest wymagana")]
     public DateOnly BirthDate { get; set; }
 }
