00bf07f4975b5f15a29738b5ac113b57147e21b5
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-19 11:24:35 +0100

    kk

---
+++ 4pa_gr2/client-side-web/cw2/src/data.ts
@@ -0,0 +1,26 @@
+type Game = {
+    id: number;
+    name: string;
+    price: number;
+    date: Date
+}
+const games: Game[] = [
+    {
+        id: 1,
+        name: 'GTA V',
+        price: 50,
+        date: new Date('2021-01-01')
+    },
+    {
+        id: 2,
+        name: 'FIFA 21',
+        price: 60,
+        date: new Date('2021-02-01')
+    },
+    {
+        id: 3,
+        name: 'Cyberpunk 2077',
+        price: 70,
+        date: new Date('2021-03-01')
+    }
+];
