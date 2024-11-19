94bf5b05ba129bfbfd1d09e5813a9b0d7f97b865
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2024-11-19 15:44:58 -0100

    kk

---
+++ 3pr_gr2/cw7/Pages/Primes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cw7.Pages
+{
+    public class PrimesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
