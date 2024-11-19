f76f9f2a4735275c309d9f95cb6822d9ff27849b
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2024-11-19 16:12:45 -0100

    hh

--- 3pr_gr2/cw7/Pages/Primes.cshtml.cs
+++ 3pr_gr2/cw7/Pages/Primes.cshtml.cs
@@ -5,7 +5,12 @@ namespace cw7.Pages
 {
     public class PrimesModel : PageModel
     {
+        [BindProperty]
+        public int Count { get; set; }
         public void OnGet()
+        {
+        }
+        public void OnPost()
         {
         }
     }
