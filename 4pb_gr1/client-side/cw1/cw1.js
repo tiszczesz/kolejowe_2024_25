be6d560f31ec5d42692dff0a1c0b8c98fc42b18e
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-25 15:06:40 +0100

    kk

--- 4pb_gr1/client-side/cw1/cw1.js
+++ 4pb_gr1/client-side/cw1/cw1.js
@@ -1,8 +1,28 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var a = 12;
 console.log(typeof a); // number
 //a = 'hello';
 var tab = [1, 2, 3, "4"];
 tab.forEach(function (element) {
     if (typeof element === 'number')
-        element.toFixed(2);
+        console.log(element.toFixed(2));
 });
+var b = 'hello'; //definicja zmiennej typu string
+var tab1 = [1, 2, 3]; //definicja tablicy liczb
+tab1.push(4); //dodanie elementu do tablicy
+var tab2 = [34, 78, 22]; //definicja tablicy liczb
+var result = __spreadArray(__spreadArray([], tab1, true), tab2, true); //połączenie dwóch tablic
+var result2 = [tab1, tab2]; //utworzenie tablicy z dwóch tablic
+function Hello(name) {
+    return "Hello ".concat(name);
+}
+console.log(Hello('World'));
+console.log(Hello(123)); //error w TS
--- 4pb_gr1/client-side/cw1/cw1.ts
+++ 4pb_gr1/client-side/cw1/cw1.ts
@@ -1,8 +1,20 @@
 let a = 12;
 console.log(typeof a); // number
 //a = 'hello';
-const tab = [1, 2, 3,"4"];
+const tab = [1, 2, 3, "4"];
 tab.forEach((element) => {
-    if(typeof element === 'number')
-        element.toFixed(2);
+    if (typeof element === 'number')
+        console.log(element.toFixed(2));
 });
+let b: string = 'hello';//definicja zmiennej typu string
+const tab1: number[] = [1, 2, 3];//definicja tablicy liczb
+tab1.push(4);//dodanie elementu do tablicy
+const tab2: Array<number> = [34, 78, 22];//definicja tablicy liczb
+const result = [...tab1, ...tab2];//połączenie dwóch tablic
+const result2: number[][] = [tab1, tab2];//utworzenie tablicy z dwóch tablic
+
+function Hello(name: string): string {
+    return `Hello ${name}`;
+}
+console.log(Hello('World'));
+//console.log(Hello(123));//error w TS
