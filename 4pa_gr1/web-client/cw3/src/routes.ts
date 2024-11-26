eec55ab5c5fca149f9b30dc22601799374f770af
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-26 11:02:45 +0100

    kk

--- 4pa_gr1/web-client/cw3/src/routes.ts
+++ 4pa_gr1/web-client/cw3/src/routes.ts
@@ -1,7 +1,12 @@
 import { IncomingMessage, ServerResponse } from "http";
 import fs from "fs";
 import { users } from "./data.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+//const __dirname = path.dirname(new URL(import.meta.url).pathname);
 export const routes = (req: IncomingMessage, res: ServerResponse) => {
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -16,7 +21,31 @@ export const routes = (req: IncomingMessage, res: ServerResponse) => {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         const html = fs.readFileSync('./public/index.html', 'utf8');
         res.end(html);
-    } else {
+    } else if (req.url?.startsWith('/public/')) {
+        const filePath = path.join(__dirname, '..', req.url);
+        const ext = path.extname(filePath);
+        let contentType = 'text/plain';
+
+        switch (ext) {
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            // Dodaj inne typy plików, jeśli potrzebujesz
+        }
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h2>File Not Found</h2>');
+            } else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content);
+            }
+        });
+    }
+    else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end(`<h2>Page Not Found</h2>`);
     }
