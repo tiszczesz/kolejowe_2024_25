b856fa440e3319b978481b721b1aac5327e9b73b
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-26 14:26:26 +0100

    kk

--- 4pa_gr2/client-side-web/cw3/src/routes.ts
+++ 4pa_gr2/client-side-web/cw3/src/routes.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
 import fs from 'fs';
+import { books } from "./data.js";
 
 export const routes = (req: IncomingMessage, res: ServerResponse) => {
     if (req.url === '/') {
@@ -14,6 +15,11 @@ export const routes = (req: IncomingMessage, res: ServerResponse) => {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         const html = fs.readFileSync('./public/index.html');
         res.write(html);
+        return res.end();
+    }
+    else if(req.url === '/books'){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(books));
         return res.end();
     }
      else {
