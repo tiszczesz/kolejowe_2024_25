93446fd274a7bac61f3c9d285328391c127b87c2
tiszczesz <tiszczesz@users.noreply.github.com>
2024-11-26 10:24:54 +0100

    kk

--- 4pa_gr1/web-client/cw3/src/routes.ts
+++ 4pa_gr1/web-client/cw3/src/routes.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http";
+import fs from "fs";
 import { users } from "./data.js";
 
 export const routes = (req: IncomingMessage, res: ServerResponse) => {
@@ -11,6 +12,10 @@ export const routes = (req: IncomingMessage, res: ServerResponse) => {
     } else if (req.url === '/api/users') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(users));
+    } else if (req.url === '/index') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const html = fs.readFileSync('./public/index.html', 'utf8');
+        res.end(html);
     } else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end(`<h2>Page Not Found</h2>`);
