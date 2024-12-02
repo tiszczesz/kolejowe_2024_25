e751ed81be160096064df037d20175d7d5e38794
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-02 14:43:37 +0100

    hh

---
+++ 3pr_gr2/cw7/Pages/PageCheck.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cw7.Pages
+{
+    public class PageCheckModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
