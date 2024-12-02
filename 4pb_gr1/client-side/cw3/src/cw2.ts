b32aa1626ae5d492774798c0d16d9b826f420497
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-02 15:23:52 +0100

    gg

--- 4pb_gr1/client-side/cw3/src/cw2.ts
+++ 4pb_gr1/client-side/cw3/src/cw2.ts
@@ -1,8 +1,19 @@
 import fs from 'node:fs';
-import { colors } from './data.js';
+import { colors, getLines, howManyLetters, saveInfo } from './data.js';
 
-const input = fs.readFileSync('input.txt', 'utf8').replace("\r","").split('\n');
+const input = fs.readFileSync('input.txt', 'utf8').replace("\r", "").split('\n');
 console.log(input);
 // fs.writeFileSync("output.txt", colors.join('-'), 'utf8');
 // console.log(input);
 // fs.appendFileSync("output.txt", "\n nowa linijka do pliku", 'utf8');
+const lines = getLines("info2.txt");
+console.log(lines);
+const info ={
+    count: lines.length, 
+    chars:lines.join('').length, 
+    words:lines.join(' ').split(' ').length,
+    letters: howManyLetters(lines),
+    vovels: lines.join('').match(/[aeiouAEIOU]/gi)?.length,
+};
+console.log(info);
+saveInfo("info.json", info);
--- 4pb_gr1/client-side/cw3/src/data.ts
+++ 4pb_gr1/client-side/cw3/src/data.ts
@@ -1,3 +1,4 @@
+
 import fs from 'fs';
 export const colors = ["red", "green", "blue", "yellow", "pink"];
 export const numbers = [1, 2, 3, 4, 5];
@@ -21,13 +22,30 @@ export const ShowWithForIn = (data: string[]) => {
     }
 }
 
-const getLines = (filename: string): string[] => {
-    const result = fs.readFileSync(filename, "utf8").replace("\r", "").split('\n');
-    return result;
+export const getLines = (filename: string): string[] => {
+    return fs.readFileSync(filename, "utf8").replace(/\r/g, "").split("\n");
+    // console.log(content);
+
 };
+
+export const howManyLetters = (data: string[]): number => {
+    let total = 0;
+    for (let line of data) {
+        for (let letter of line) {
+            if (letter.match(/[a-zA-Z]/)) {
+                total++;
+            }
+        }        
+    }
+    return total;
+}
 Object.keys(student).forEach(key => {
     console.log(key);
 });
 Object.values(student).forEach(value => {
     console.log(value);
 });
+export const saveInfo = (filename: string, data: any) => {
+    const content = JSON.stringify(data, null, 2);
+    fs.writeFileSync(filename, content, "utf8");
+};
