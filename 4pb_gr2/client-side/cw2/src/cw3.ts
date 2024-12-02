efccc73b94f722a4b22b101b6aeffa9f987d3cc4
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-02 19:04:58 +0100

    kk

--- 4pb_gr2/client-side/cw2/src/cw3.ts
+++ 4pb_gr2/client-side/cw2/src/cw3.ts
@@ -8,7 +8,8 @@ const rl = readline.createInterface({
 async function main() {
     const name = await rl.question('What is your name? ');
     const age = await rl.question('How old are you? ');
-    console.log(`Hello, ${name}! age: ${age}`);
+    const length = await rl.question('dlugosc czegos ? ');
+    console.log(`Hello, ${name}! age: ${age} length: ${length}`);
     rl.close();
 }
 main();
