7d18a993dc9eb9e012d76e733f7e5e14f91647dd
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-03 09:22:39 +0100

    kk

--- 4pa_gr1/web-client/cw3/src/routes.ts
+++ 4pa_gr1/web-client/cw3/src/routes.ts
@@ -25,6 +25,42 @@ export const routes = (req: IncomingMessage, res: ServerResponse) => {
         const html = fs.readFileSync('./public/index.html', 'utf8');
         return res.end(html);
     }
+    if (req.url === '/form') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const html = fs.readFileSync('./public/form.html', 'utf8');
+        return res.end(html);
+    }
+    if (req.url === '/result' && req.method === 'POST') {
+        const body: any[] = [];
+        req.on('data', (chunk: any) => {
+           // console.log(chunk.toString());
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+           // console.log(parsedBody);
+            // const name = parsedBody.split('=')[1];
+            // console.log(name);
+            const result = parsedBody.split('&').map((item) => {
+                return item.split('=')[1];
+            });
+            console.log(parsedBody);    
+            console.log(result); 
+            if(result.length<4 || result[0] === '' || result[1] === '' || result[2] === ''){
+                res.writeHead(302, { 'Location': '/form' });
+                return res.end();
+            }
+            const user = {
+                firstname: result[0],
+                lastname: result[1],
+                age: result[2],
+                data:result[3]
+            } 
+            fs.appendFileSync('./data.txt', JSON.stringify(user)+'\n');      
+            res.writeHead(302, { 'Location': '/form' });
+            return res.end();
+        });
+    }
     if (req.url?.startsWith('/public/')) {
         const filePath = path.join(__dirname, '..', req.url);
         const ext = path.extname(filePath);
