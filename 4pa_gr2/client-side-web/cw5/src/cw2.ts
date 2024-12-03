455383cdf030ca1d1659fa839e4baf8b042572d3
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-03 14:26:02 +0100

    kk

--- 4pa_gr2/client-side-web/cw5/src/cw2.ts
+++ 4pa_gr2/client-side-web/cw5/src/cw2.ts
@@ -1,3 +1,32 @@
 // podaj a
 // podaj b
-// nwd(a,b) = ? funkcja rekurencyjna
+// nwdRek(a,b) = ? funkcja rekurencyjna
+// nwdIter(a,b) = ? funkcja iteracyjna
+import readLine from "readline/promises";
+const read = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const nwdRek = (a: number, b: number): number => {
+    if (b === 0) {
+        return a;
+    }
+    return nwdRek(b, a % b);
+};
+const nwdIter = (a: number, b: number): number => {
+    while (b !== 0) {
+        let c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+};
+const main = async () => {
+
+    let a = parseInt(await read.question('Podaj liczbę a: '));
+    let b = parseInt(await read.question('Podaj liczbę b: '));
+    read.close();
+    console.log(`NWDRek(${a},${b}) = ${nwdRek(a, b)}`);
+    console.log(`NWDIter(${a},${b}) = ${nwdIter(a, b)}`);
+}; 
+main();
