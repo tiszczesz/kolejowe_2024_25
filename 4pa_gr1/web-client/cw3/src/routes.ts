79d8d226f821301bf3685c6a491390bff4784b69
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-03 08:43:10 +0100

    kk

--- 4pa_gr1/web-client/cw3/src/routes.ts
+++ 4pa_gr1/web-client/cw3/src/routes.ts
@@ -10,18 +10,22 @@ const __dirname = path.dirname(__filename);
 export const routes = (req: IncomingMessage, res: ServerResponse) => {
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`<h2>Hello, World!</h2>`);
-    } else if (req.url === '/about') {
+        return res.end(`<h2>Hello, World!</h2>`);
+    }
+    if (req.url === '/about') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`<h2>About Us</h2>`);
-    } else if (req.url === '/api/users') {
+        return res.end(`<h2>About Us</h2>`);
+    }
+    if (req.url === '/api/users') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
-    } else if (req.url === '/index') {
+        return res.end(JSON.stringify(users));
+    }
+    if (req.url === '/index') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         const html = fs.readFileSync('./public/index.html', 'utf8');
-        res.end(html);
-    } else if (req.url?.startsWith('/public/')) {
+        return res.end(html);
+    }
+    if (req.url?.startsWith('/public/')) {
         const filePath = path.join(__dirname, '..', req.url);
         const ext = path.extname(filePath);
         let contentType = 'text/plain';
@@ -38,16 +42,16 @@ export const routes = (req: IncomingMessage, res: ServerResponse) => {
         fs.readFile(filePath, (err, content) => {
             if (err) {
                 res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.end('<h2>File Not Found</h2>');
+                return res.end('<h2>File Not Found</h2>');
             } else {
                 res.writeHead(200, { 'Content-Type': contentType });
-                res.end(content);
+                return res.end(content);
             }
         });
     }
     else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end(`<h2>Page Not Found</h2>`);
+       return  res.end(`<h2>Page Not Found</h2>`);
     }
 
 }
