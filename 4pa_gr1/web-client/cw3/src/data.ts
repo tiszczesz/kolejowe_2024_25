606b755271a001456b1936fa1fd662f2f8ec41f8
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-03 10:42:11 +0100

    kk

--- 4pa_gr1/web-client/cw3/src/data.ts
+++ 4pa_gr1/web-client/cw3/src/data.ts
@@ -7,5 +7,7 @@ export type User = {
 export const users: User[] = [
     { name: 'John', age: 25 },
     { name: 'Jane', age: 27 },
-    { name: 'Jim', age: 30 }
+    { name: 'Jim', age: 30 },
+    { name: 'Olo', age: 22 },
+
 ];
