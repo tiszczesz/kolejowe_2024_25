9a9fc1d47e661f97b743e2e6827d1c4adf104fcf
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2024-12-03 16:12:54 -0100

    kk

--- 3pr_gr2/cw8_sqlite/Models/ProductsRepo.cs
+++ 3pr_gr2/cw8_sqlite/Models/ProductsRepo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using Microsoft.Data.Sqlite;
 namespace cw8_sqlite.Models;
 
@@ -31,4 +32,49 @@ public class ProductsRepo
 
         return products;
     }
+
+    public Product? GetProductById(int id)
+    {
+        using SqliteConnection connection = new(_connString);
+        SqliteCommand command = connection.CreateCommand();
+       // command.CommandText = $"SELECT * FROM products WHERE Id = {id}";
+        command.CommandText = "SELECT * FROM products WHERE Id = @id";
+        command.Parameters.AddWithValue("@id", id);
+        connection.Open();
+        using SqliteDataReader reader = command.ExecuteReader();
+        if(reader.HasRows){
+            reader.Read();
+            return new Product{
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Price = reader.GetDecimal(2),
+                Type = reader.GetString(3)
+            };
+        }
+            return null;
+    }
+
+    public void AddProduct(Product product)
+    {
+        using SqliteConnection connection = new(_connString);
+        SqliteCommand command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO products (Name, Price, Type) "
+               +" VALUES (@name, @price, @type)";
+        command.Parameters.AddWithValue("@name", product.Name);
+        command.Parameters.AddWithValue("@price", 
+                 product.Price?.ToString(CultureInfo.InvariantCulture));
+        command.Parameters.AddWithValue("@type", product.Type);
+        connection.Open();
+        command.ExecuteNonQuery();
+    }
+    public void DeleteProduct(int id){
+        using SqliteConnection connection = new(_connString);
+        SqliteCommand command = connection.CreateCommand();
+        command.CommandText = "DELETE FROM products WHERE Id = @id";
+        command.Parameters.AddWithValue("@id", id);
+        connection.Open();
+        command.ExecuteNonQuery();
+    }
 }
+
+
--- 3pr_gr2/cw8_sqlite/Program.cs
+++ 3pr_gr2/cw8_sqlite/Program.cs
@@ -7,3 +7,36 @@ foreach(var product in products)
     Console.WriteLine($"{product.Id}\t {product.Name}\t "
            + $" {product.Price}\t {product.Type}");
 }
+//uzytkownik wprowadza id produktu w osobnej funkcji w ProductsRepo
+//wyswietlamy produkt o podanym id
+Console.WriteLine("Podaj id produktu: ");
+int id = Convert.ToInt32(Console.ReadLine());
+Product? findProduct = repo.GetProductById(id);
+if(findProduct != null)
+{
+    Console.WriteLine($"{findProduct.Id}\t {findProduct.Name}\t "
+           + $" {findProduct.Price}\t {findProduct.Type}");
+}
+else
+{
+    Console.WriteLine("Brak produktu o podanym id");
+}
+Product newProduct = CreateProduct();
+repo.AddProduct(newProduct);
+
+//napisz metodę która prosi uzytkownika o podanie danych produktu
+// i generuj nowy obiekt Product nazwa cena typ
+
+Product CreateProduct(){
+    Console.WriteLine("Podaj nazwę produktu: ");
+    string? name = Console.ReadLine();
+    Console.WriteLine("Podaj cenę produktu: ");
+    decimal price = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("Podaj typ produktu: ");
+    string? type = Console.ReadLine();
+    return new Product{
+        Name = name,
+        Price = price,
+        Type = type
+    };
+}
