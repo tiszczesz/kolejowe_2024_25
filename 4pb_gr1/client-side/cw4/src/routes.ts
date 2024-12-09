80ecb9cf799c8eac2e85a4a9c1b888da51e35759
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-09 16:08:00 +0100

    kk

--- 4pb_gr1/client-side/cw4/src/routes.ts
+++ 4pb_gr1/client-side/cw4/src/routes.ts
@@ -5,11 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { url } from "inspector";
 
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
-console.log(import.meta.url);
-console.log(_filename);
-console.log(_dirname);
+
 
 
 
@@ -31,48 +27,24 @@ export const routes = (req: IncomingMessage, res: ServerResponse) => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         return res.end(JSON.stringify(books[0]));
     }
-    if(req.url === '/form'){
+    if (req.url === '/form') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         const html = fs.readFileSync('./public/form.html');
         return res.end(html);
     }
-    if(req.url === '/result' && req.method === 'POST'){
-        const body:any[] = [];
-        req.on('data',(chunk:any)=>{
+    if (req.url === '/result' && req.method === 'POST') {
+        const body: any[] = [];
+        req.on('data', (chunk: any) => {
             body.push(chunk)
         });
-        return req.on('end',()=>{
+        return req.on('end', () => {
             const parseBody = Buffer.concat(body).toString();
             console.log(parseBody);
-            
+
         });
     }
     if (req.url?.startsWith('/public/')) {
-        const filePath = path.join(_dirname, '..', req.url);
-        console.log(filePath);
-        let contentType = 'text/plain';
-        let ext = path.extname(filePath); //rozszerzenia pobieranych plików statycznych
-        switch (ext) {
-            case '.css':
-                contentType = 'text/css'; break;
-            case '.jpg':
-                contentType = 'image/jpeg'; break;
-            case '.jpeg':
-                contentType = 'image/jpeg'; break;
-            case '.png':
-                contentType = 'image/png'; break;
-            case '.js':
-                contentType = 'text/javascript'; break;
-            //inne typy
-        }
-        fs.readFile(filePath, (err, content) => {
-            if (err) {
-                res.writeHead(404, { "content-type": 'text/html' });
-                return res.end('File not found');
-            }
-            res.writeHead(200, { 'content-type': contentType });
-            return res.end(content);
-        });
+        useStaticFiles(req, res);
 
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -80,4 +52,37 @@ export const routes = (req: IncomingMessage, res: ServerResponse) => {
     }
 
 
+}
+
+function useStaticFiles(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const _filename = fileURLToPath(import.meta.url);
+    const _dirname = path.dirname(_filename);
+    console.log(import.meta.url);
+    console.log(_filename);
+    console.log(_dirname);
+    const filePath = path.join(_dirname, '..', req.url!);
+    console.log(filePath);
+    let contentType = 'text/plain';
+    let ext = path.extname(filePath); //rozszerzenia pobieranych plików statycznych
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css'; break;
+        case '.jpg':
+            contentType = 'image/jpeg'; break;
+        case '.jpeg':
+            contentType = 'image/jpeg'; break;
+        case '.png':
+            contentType = 'image/png'; break;
+        case '.js':
+            contentType = 'text/javascript'; break;
+        //inne typy
+    }
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404, { "content-type": 'text/html' });
+            return res.end('File not found');
+        }
+        res.writeHead(200, { 'content-type': contentType });
+        return res.end(content);
+    });
 }
