00ac47278dcd8db2822f64e5535a2c370ea34708
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-09 17:36:25 +0100

    kk

---
+++ 4pb_gr2/client-side/cw3/src/routes.ts
@@ -0,0 +1,73 @@
+import { IncomingMessage, ServerResponse } from "http";
+import fs from "fs";
+import { games } from "./data.js";
+import { fileURLToPath } from "url";
+import path from "path";
+
+
+export const routes = (req: IncomingMessage, res: ServerResponse) => {
+    //console.log(__dirname);
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('Hello World!');
+        return res.end();
+    } if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h3>About Us</h3>');
+        return res.end();
+    }
+    if (req.url === '/games') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(games));
+        return res.end();
+    }
+    if (req.url === '/index') {
+        const html = fs.readFileSync('./public/index.html');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(html);
+        return res.end();
+    }
+    if (req.url?.startsWith('/public/')) {
+        useStaticFiles(req, res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('Not Found');
+        return res.end();
+    }
+
+}
+
+function useStaticFiles(req: IncomingMessage, res: ServerResponse) {
+    const _filename = fileURLToPath(import.meta.url);
+    console.log(_filename);
+    const _dirname = path.dirname(_filename);
+    const _path = path.join(_dirname, '..', req.url!);
+    console.log(_path);
+    const ext = path.extname(_path);
+    let contentType = 'text/plain';
+    switch (ext) {
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+    }
+    console.log(contentType);
+    fs.readFile(_path, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.write('Not Found');
+            return res.end();
+        }
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.write(data);
+        return res.end();
+    });
+}
--- 4pb_gr2/client-side/cw3/src/server.ts
+++ 4pb_gr2/client-side/cw3/src/server.ts
@@ -1,20 +1,8 @@
-import http, { IncomingMessage, ServerResponse } from 'http';
+import http from 'http';
+import { routes } from './routes.js';
 
 
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('Hello World!');
-        return res.end();
-    } if (req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<h3>About Us</h3>');
-        return res.end();
-    }
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.write('Not Found');
-    return res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
