fc08c704b3fd185235abf12b9fc3850c36d695eb
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-09 13:33:22 +0100

    kk

---
+++ 4pb_gr1/client-side/cw4/src/data.ts
@@ -0,0 +1,1 @@
+export const data = ["a", "b", "c"];
---
+++ 4pb_gr1/client-side/cw4/src/server.ts
@@ -0,0 +1,17 @@
+import http, { IncomingHttpHeaders, IncomingMessage, ServerResponse } from 'http';
+
+const server = http.createServer(
+    (req: IncomingMessage, res: ServerResponse) => {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            return res.end('Hello World\n');
+        }
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        return res.end('Not Found\n');
+
+    }
+);
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
