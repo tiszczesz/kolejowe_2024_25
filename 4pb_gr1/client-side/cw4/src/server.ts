53a96dac9a9ec087043c033ffb9d8524153275c3
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-09 13:55:12 +0100

    kk

--- 4pb_gr1/client-side/cw4/src/server.ts
+++ 4pb_gr1/client-side/cw4/src/server.ts
@@ -1,4 +1,4 @@
-import http, { IncomingHttpHeaders, IncomingMessage, ServerResponse } from 'http';
+import http, { IncomingMessage, ServerResponse } from 'http';
 
 const server = http.createServer(
     (req: IncomingMessage, res: ServerResponse) => {
