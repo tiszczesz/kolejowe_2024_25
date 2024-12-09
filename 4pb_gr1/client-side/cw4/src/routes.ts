644363e7236538c7b41690abd3d029ce7f89917f
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-09 14:36:28 +0100

    ll

--- 4pb_gr1/client-side/cw4/src/routes.ts
+++ 4pb_gr1/client-side/cw4/src/routes.ts
@@ -1,6 +1,12 @@
 import { IncomingMessage, ServerResponse } from "http";
 import fs from "fs";
 import { Book,books } from "./data.js";
+import path from "path";
+
+const _filename = new URL(import.meta.url).pathname;
+const _dirname = path.dirname(_filename);
+console.log(_dirname);
+console.log(_filename);
 
 export const routes =  (req: IncomingMessage, res: ServerResponse) => {
     if (req.url === '/') {
@@ -15,6 +21,13 @@ export const routes =  (req: IncomingMessage, res: ServerResponse) => {
     if(req.url === '/api/books') {    
         res.writeHead(200, { 'Content-Type': 'application/json' });
         return res.end(JSON.stringify(books));        
+    }
+    if(req.url === '/api/books/1') {    
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify(books[0]));        
+    }
+    if(req.url?.startsWith('/public/')){
+
     }
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     return res.end('Not Found\n');
