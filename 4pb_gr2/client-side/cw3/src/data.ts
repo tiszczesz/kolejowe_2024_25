70f77935f37c5c6c2e3bd361beeedba3d18fe3fb
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-09 16:50:04 +0100

    kk

--- 4pb_gr2/client-side/cw3/src/data.ts
+++ 4pb_gr2/client-side/cw3/src/data.ts
@@ -1,1 +1,27 @@
 export const data: string[] = ['a', 'b', 'c'];
+export interface Game{
+    name: string;
+    price: number;
+    genre: string;
+    year: number;
+}
+export const games: Game[] = [
+    {
+        name: 'GTA V',
+        price: 60,
+        genre: 'Action',
+        year: 2013
+    },
+    {
+        name: 'The Witcher 3',
+        price: 50,
+        genre: 'RPG',
+        year: 2015
+    },
+    {
+        name: 'Cyberpunk 2077',
+        price: 60,
+        genre: 'RPG',
+        year: 2020
+    }
+];
