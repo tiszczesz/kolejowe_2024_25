87fab4da1a0d7c08ae0de2b0c9fe99dcd8bb4a29
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-09 18:48:31 +0100

    kk

--- 4pb_gr2/client-side/cw3/src/server.ts
+++ 4pb_gr2/client-side/cw3/src/server.ts
@@ -1,9 +1,26 @@
 import http from 'http';
 import { routes } from './routes.js';
+import { IncomingMessage, ServerResponse } from 'http';
 
+//const server = http.createServer(routes);
 
-const server = http.createServer(routes);
+const enableCors = (req: IncomingMessage, res: ServerResponse) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return true;
+    }
+    return false;
+};
 
+const server = http.createServer((req, res) => {
+    if (!enableCors(req, res)) {
+        routes(req, res);
+    }
+});
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
