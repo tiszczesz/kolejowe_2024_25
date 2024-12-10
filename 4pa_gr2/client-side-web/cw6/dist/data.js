5efaa1ef12a4c31bc0042efd409c75a082f9534f
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-10 12:33:11 +0100

    kk

--- 4pa_gr2/client-side-web/cw6/dist/data.js
+++ 4pa_gr2/client-side-web/cw6/dist/data.js
@@ -3,3 +3,75 @@ export const colors = [
     'black', 'white', 'gray'
 ];
 export const size = [10, 12, 14, 18, 24, 32, 38];
+export const products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        price: 100,
+        description: 'Description of product 1',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        price: 200,
+        description: 'Description of product 2',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        price: 300,
+        description: 'Description of product 3',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        price: 400,
+        description: 'Description of product 4',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 5,
+        name: 'Product 5',
+        price: 500,
+        description: 'Description of product 5',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 6,
+        name: 'Product 6',
+        price: 600,
+        description: 'Description of product 6',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 7,
+        name: 'Product 7',
+        price: 700,
+        description: 'Description of product 7',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 8,
+        name: 'Product 8',
+        price: 800,
+        description: 'Description of product 8',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 9,
+        name: 'Product 9',
+        price: 900,
+        description: 'Description of product 9',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 10,
+        name: 'Product 10',
+        price: 1000,
+        description: 'Description of product 10',
+        image: 'https://via.placeholder.com/150'
+    }
+];
