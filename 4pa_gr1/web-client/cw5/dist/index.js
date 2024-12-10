25443ec0f94723e0f0e9014fba298ba121754afd
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-10 10:51:00 +0100

    hh

--- 4pa_gr1/web-client/cw5/dist/index.js
+++ 4pa_gr1/web-client/cw5/dist/index.js
@@ -1,4 +1,5 @@
 import { colors } from "./data.js";
+import { posts } from "./dataposts.js";
 function generSelect(colors) {
     let html = `<select id="colors">`;
     for (let color of colors) {
@@ -7,10 +8,28 @@ function generSelect(colors) {
     html += `</select>`;
     return html;
 }
+function generPosts(posts) {
+    let html = ``;
+    for (let post of posts) {
+        html += `<div class="card m-2">
+                    <div class="card-body">
+                        <div
+                        style="text-decoration: underline;"
+                         class=" p-2 card-title bg-warning-subtle">${post.title}</div>
+                        <div class="card-text">${post.body}</div>
+                    </div>
+                </div>`;
+    }
+    return html;
+}
 const root = document.querySelector("#root");
 if (root) {
     root.innerHTML = generSelect(colors);
     document.querySelector("#colors")?.addEventListener("change", (e) => {
         document.body.style.backgroundColor = e.target?.value;
     });
+}
+const root2 = document.querySelector("#root2");
+if (root2) {
+    root2.innerHTML = generPosts(posts);
 }
--- 4pa_gr1/web-client/cw5/src/index.ts
+++ 4pa_gr1/web-client/cw5/src/index.ts
@@ -1,11 +1,27 @@
 import { colors } from "./data.js";
+import { Post, posts } from "./dataposts.js";
 
 function generSelect(colors: string[]): string {
-   let html = `<select id="colors">`;
+    let html = `<select id="colors">`;
     for (let color of colors) {
-         html += `<option value="${color}">${color}</option>`;
+        html += `<option value="${color}">${color}</option>`;
     }
     html += `</select>`;
+    return html;
+}
+function generPosts(posts: Post[]): string {
+    let html = ``;
+    for (let post of posts) {
+        html += `<div class="card m-2">
+                    <div class="card-body">
+                        <div
+                        style="text-decoration: underline;"
+                         class=" p-2 card-title bg-warning-subtle">${post.title}</div>
+                        <div class="card-text">${post.body}</div>
+                    </div>
+                </div>`;
+    }
+
     return html;
 }
 
@@ -15,4 +31,8 @@ if (root) {
     document.querySelector<HTMLSelectElement>("#colors")?.addEventListener("change", (e) => {
         document.body.style.backgroundColor = (e.target as HTMLSelectElement)?.value;
     });
+}
+const root2 = document.querySelector<HTMLDivElement>("#root2");
+if (root2) {
+    root2.innerHTML = generPosts(posts);
 }
