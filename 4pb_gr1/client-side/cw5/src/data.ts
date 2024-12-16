d52dc65e125cd3b3e78244fa105cff9f925a7790
tiszczesz <tiszczesz@users.noreply.github.com>
2024-12-16 13:12:29 +0100

    kk

---
+++ 4pb_gr1/client-side/cw5/src/data.ts
@@ -0,0 +1,18 @@
+export type User = {
+    id:number,
+    login: string,
+    password: string,
+    date: Date
+}
+export const users: User[] = [
+    { id: 1, login: 'user1', password: 'password1', date: new Date() },
+    { id: 2, login: 'user2', password: 'password2', date: new Date() },
+    { id: 3, login: 'user3', password: 'password3', date: new Date() },
+    { id: 4, login: 'user4', password: 'password4', date: new Date() },
+    { id: 5, login: 'user5', password: 'password5', date: new Date() },
+    { id: 6, login: 'user6', password: 'password6', date: new Date() },
+    { id: 7, login: 'user7', password: 'password7', date: new Date() },
+    { id: 8, login: 'user8', password: 'password8', date: new Date() },
+    { id: 9, login: 'user9', password: 'password9', date: new Date() },
+    { id: 10, login: 'user10', password: 'password10', date: new Date() }
+];
