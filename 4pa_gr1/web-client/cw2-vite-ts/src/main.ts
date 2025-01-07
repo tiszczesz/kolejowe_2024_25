50984c14d47e10eed4a5a1ab90a5842991a6261b
tiszczesz <tiszczesz@users.noreply.github.com>
2025-01-07 10:56:05 +0100

    Update main.ts

--- 4pa_gr1/web-client/cw2-vite-ts/src/main.ts
+++ 4pa_gr1/web-client/cw2-vite-ts/src/main.ts
@@ -3,13 +3,15 @@ import './style.css';
 import { MyHeader } from './components/MyHeader';
 import { getTodos } from './data/getTodos';
 import { TodosTable } from './components/TodosTable';
+import { MyRectColor } from './components/MyRectColor';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app) {
   app.appendChild(MyHeader('<h1>Witamy w roku 2025</h1>', "myHeader"));
-  getTodos().then(todos => {
-    app.appendChild(TodosTable(todos));
-  });
-
+  const todos = await getTodos();
+  app.appendChild(TodosTable(todos));
+  app.appendChild(MyRectColor('green'));
+  app.appendChild(MyRectColor('red'));
+  app.appendChild(MyRectColor('blue'));
 }
