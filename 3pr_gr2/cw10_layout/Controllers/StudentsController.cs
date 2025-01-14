0df06f78631796758d6c0b8a12e0f73595a819dd
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-01-14 16:09:56 -0100

    kk

--- 3pr_gr2/cw10_layout/Controllers/StudentsController.cs
+++ 3pr_gr2/cw10_layout/Controllers/StudentsController.cs
@@ -1,13 +1,20 @@
+using cw10_layout.Models;
+using cw10_layout.Models.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw10_layout.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly IStudentRepo _studentRepo;
+        public StudentsController()
+        {
+            _studentRepo = new FakeStudentRepo();
+        }
         // GET: StudentsController
         public ActionResult List()
         {
-            return View();
+            return View(_studentRepo.GetAllStudents());
         }
         public IActionResult Create()
         {
