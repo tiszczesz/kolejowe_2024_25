056e2b32d736fc2b804e53a3b65461ead58c4213
tiszczesz <tiszczesz@users.noreply.github.com>
2025-01-14 10:48:21 +0100

    kk

---
+++ 4pa_gr1/web-client/cw7-react/src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+      <h1 className="header">ala ma kota</h1>
+      <div>asaasdasdas</div>
+      </header>
+      <section>
+         <ul>
+        <li>qe</li>
+        <li>qeqe </li>
+        <li>qweqwe qweeq</li>
+      </ul>
+      </section>
+     
+      <select name="sel" id="sel">
+        <option value="white">white</option>
+        <option value="red">red</option>
+        <option value="green">green</option>
+      </select>
+    </main>
+  );
+}
+
+export default App;
