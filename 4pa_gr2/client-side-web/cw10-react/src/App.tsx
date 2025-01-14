981b92e82c785a3879b85c4f4975a8219876ec40
tiszczesz <tiszczesz@users.noreply.github.com>
2025-01-14 14:34:06 +0100

    jj

---
+++ 4pa_gr2/client-side-web/cw10-react/src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  const text = "tttttt";
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum corrupti
+        architecto, quas minima laboriosam, id, necessitatibus veniam
+        consequuntur magni consectetur quod! Quia tempora doloremque animi iste
+        unde commodi eveniet atque!
+      </p>
+      <div>{text}</div>
+      <ol>
+        <li>sdfs</li>
+        <li>asdad</li>
+        <li>sfsfsf</li>
+      </ol>
+      <div>ala ma kota</div>
+    </>
+  );
+}
+
+export default App;
