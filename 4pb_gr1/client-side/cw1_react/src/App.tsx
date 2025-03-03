5dd221b4d751bdb190b6b1720a7a2cdb597cac22
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-03 14:29:18 +0100

    kk

--- 4pb_gr1/client-side/cw1_react/src/App.tsx
+++ 4pb_gr1/client-side/cw1_react/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Message from './components/Message'
+import MyComponent1 from './components/MyComponent1'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+
+  return (  
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Aktualny czas: {new Date().toLocaleTimeString()}</p>  
+      <p>d ad asd asd ad</p>
+      <hr />
+      <MyComponent1 />
+      <Message />
+      <Message />
     </>
   )
 }
---
+++ 4pb_gr1/client-side/cw1_react/src/components/Message.tsx
@@ -0,0 +1,13 @@
+type Props = {}
+
+function Message({}: Props) {
+  return (
+    <div className="message">
+        <h3>Adam Nowak</h3>
+        <h4>Klasa 1a</h4>
+        <h5>Rok szkolny 2024/25</h5>
+    </div>
+  )
+}
+
+export default Message
