6f3db29d5c3a6ae09f50af1b4cf087341f1aefcd
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-03 17:05:07 +0100

    kk

---
+++ 4pb_gr2/client-side/cw1_react/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="ddd">React: </h2>
+      <h4>ASasASss</h4>
+      <hr />
+      <ol>
+        <li>2342</li>
+        <li>wqwq</li>
+        <li>ssdf</li>
+        <li>ewewewe</li>
+      </ol>
+    </>
+  );
+}
+
+export default App;
