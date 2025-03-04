dd808e71521475ae7991a6b20603ac8542246d8e
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-04 13:06:02 +0100

    kk

---
+++ 4pa_gr2/client-side-web/cw12-react/src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="container">
+      <h1>Hello from React and bootstrap</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ 4pa_gr2/client-side-web/cw12-react/src/data/cars.ts
@@ -0,0 +1,12 @@
+export type Car = {   
+    model: string
+    year: number
+    color: string
+    price: number
+}
+export const cars: Car[] = [
+    { model: 'Toyota', year: 2020, color: 'red', price: 20000 },
+    { model: 'Fiat', year: 2012, color: 'blue', price: 10000 },
+    { model: 'Volvo', year: 2019, color: 'green', price: 35000 },
+    { model: 'Citroen', year: 2021, color: 'orange', price: 12000 }
+];
