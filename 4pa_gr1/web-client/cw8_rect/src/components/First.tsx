3cf0d15d090a3518f56d89aa17d8b9aed642c404
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-04 09:14:17 +0100

    jj

--- 4pa_gr1/web-client/cw8_rect/src/components/First.tsx
+++ 4pa_gr1/web-client/cw8_rect/src/components/First.tsx
@@ -4,8 +4,12 @@ type FirstProps = {
 }
 export default function First(props: FirstProps) {
     return(
-        <h5 style={{color:props.firstColor}}>{props.content}</h5>
+        <>
+            <h5 style={{color:props.firstColor}}>{props.content}</h5>
+            <p>Ala sasdasd adad</p>
+        </>
     )
 }
-//napisz komponent Info, który przyjmuje propsy title, description, color
+//napisz komponent Info, który przyjmuje propsy title, 
+// description, color
 //i wyświetla te informacje w formie diva w ramce
