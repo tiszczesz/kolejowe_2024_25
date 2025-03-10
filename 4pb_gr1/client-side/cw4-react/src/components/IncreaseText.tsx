475bead33694a98b61f59c192921d1bde871925d
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 16:09:50 +0100

    jj

--- 4pb_gr1/client-side/cw4-react/src/components/IncreaseText.tsx
+++ 4pb_gr1/client-side/cw4-react/src/components/IncreaseText.tsx
@@ -1,21 +1,22 @@
-import  { useState } from 'react'
-
-
+import { useState } from "react";
 
 const IncreaseText = () => {
-    const [content,setContent] = useState("Hello World");
-    console.log("Renderowanie komponentu IncreaseText");
-    
-  return (
-    <div onClick={
-        ()=>{
-            console.log("Kliknięto tekst");
-            // (e.target as HTMLDivElement).innerHTML += 'a' ;
-            setContent(content + 'a')
-            
-        } //tutaj powinna być funkcja zwiększająca rozmiar tekstu
-    }>{content}</div>
-  )
-}
+  const [content, setContent] = useState("Hello World");
+  console.log("Renderowanie komponentu IncreaseText");
 
-export default IncreaseText
+  return (
+    <div
+      onClick={
+        () => {
+          console.log("Kliknięto tekst");
+          // (e.target as HTMLDivElement).innerHTML += 'a' ;
+          setContent(content + "a");
+        } //tutaj powinna być funkcja zwiększająca rozmiar tekstu
+      }
+    >
+      {content}
+    </div>
+  );
+};
+
+export default IncreaseText;
