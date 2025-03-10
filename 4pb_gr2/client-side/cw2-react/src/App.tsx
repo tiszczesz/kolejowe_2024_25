ac857ae4c1258ab9e61c12d0c4b571d9736add19
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 16:38:35 +0100

    kk

---
+++ 4pb_gr2/client-side/cw2-react/src/App.tsx
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  const names = ["Jan", "Anna", "Maria", "Piotr", "Zofia", "Krzysztof"];
+  // const listNames = names.map((name, index) => <li key={index}>{name}</li>);
+  // console.log(listNames);
+  return (
+    <div className="container">
+      <h1>Ćwiczenie 2</h1>
+      <ul>
+        {/* <li>{names[0]}</li>
+        <li>{names[1]}</li>
+        <li>{names[2]}</li>
+        <li>{names[3]}</li> */}
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
