8bbd7648cb732ce145e15c69dec9ab0a161bd2ae
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 15:52:04 +0100

    jj

--- 4pb_gr1/client-side/cw4-react/src/App.tsx
+++ 4pb_gr1/client-side/cw4-react/src/App.tsx
@@ -1,19 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  //uzycie hooka useState do przechowywania koloru i reakcji na kliknięcie 
+  // wymusza renderowanie komponentu
+  const [myColor, setMyColor] = useState("noname");
+  
   const colors = ["white", "red", "green", "blue", "yellow", "purple"];
   console.log("Renderowanie komponentu App");
-  
+ // let myColor = "noname"; //zła droga
+  //musimy wymusić ponowne renderowanie komponentu
   return (
-    <div className="container">
+    <div className="container" style={{backgroundColor: myColor}}>
       <h1>Proste operacje na elemencie JSX</h1>
       <ul>
         {colors.map((color, index) => (
-          <li style={{cursor:"pointer"}} key={index}>{color}</li>
+          <li style={{ cursor: "pointer" }} key={index} 
+          onClick={(e)=>{
+            console.log(`Kliknięto kolor ${color}`);
+            console.log((e.target as HTMLLIElement).innerHTML);
+            setMyColor((e.target as HTMLLIElement).innerHTML);
+            console.log(myColor);
+          }}>
+            {color}
+          </li>
         ))}
       </ul>
+      {myColor}
     </div>
   );
 }
