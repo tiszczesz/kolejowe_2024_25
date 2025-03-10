c94428292371cabec99edc09730ceca951c60239
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 14:37:49 +0100

    hh

--- 4pb_gr1/client-side/cw3-react/src/components/User.tsx
+++ 4pb_gr1/client-side/cw3-react/src/components/User.tsx
@@ -1,16 +1,16 @@
-import { MouseEvent } from 'react';
+import { ComponentProps, MouseEvent, PropsWithoutRef } from 'react';
 import { type UserType } from '../data/users';
 
 type Props = {
     user: UserType;
     handleClick: (e: MouseEvent<HTMLDivElement>) => void;
-}
+} & ComponentProps<'div'>;
 
 const User = ({user,handleClick}: Props) => {
     
 
     return (
-        <div onClick={(e)=>handleClick(e)} className={user.isLogged ? "green" : "red"}>{user.name}</div>
+        <div style={{cursor:'pointer'}} onClick={(e)=>handleClick(e)} className={user.isLogged ? "green" : "red"}>{user.name}</div>
     )
 }
 export default User
