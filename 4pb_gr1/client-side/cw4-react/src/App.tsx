fab497d2fcbae8c8403905afb7fef6f73a6b15c6
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 16:02:55 +0100

    kk

--- 4pb_gr1/client-side/cw4-react/src/App.tsx
+++ 4pb_gr1/client-side/cw4-react/src/App.tsx
@@ -2,12 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import { useState } from "react";
+import IncreaseText from "./components/IncreaseText";
 
 function App() {
   //uzycie hooka useState do przechowywania koloru i reakcji na kliknięcie 
   // wymusza renderowanie komponentu
   const [myColor, setMyColor] = useState("noname");
-  
+
   const colors = ["white", "red", "green", "blue", "yellow", "purple"];
   console.log("Renderowanie komponentu App");
  // let myColor = "noname"; //zła droga
@@ -29,6 +30,8 @@ function App() {
         ))}
       </ul>
       {myColor}
+      <hr />
+      <IncreaseText />
     </div>
   );
 }
---
+++ 4pb_gr1/client-side/cw4-react/src/components/IncreaseText.tsx
@@ -0,0 +1,21 @@
+import  { useState } from 'react'
+
+
+
+const IncreaseText = () => {
+    const [content,setContent] = useState("Hello World");
+    console.log("Renderowanie komponentu IncreaseText");
+    
+  return (
+    <div onClick={
+        ()=>{
+            console.log("Kliknięto tekst");
+            // (e.target as HTMLDivElement).innerHTML += 'a' ;
+            setContent(content + 'a')
+            
+        } //tutaj powinna być funkcja zwiększająca rozmiar tekstu
+    }>{content}</div>
+  )
+}
+
+export default IncreaseText
