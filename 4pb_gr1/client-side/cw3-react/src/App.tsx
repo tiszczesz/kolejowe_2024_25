3d34df393aab0e0a8ee8fbd552e6790301162b6b
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 13:15:15 +0100

    add user list to App component

--- 4pb_gr1/client-side/cw3-react/src/App.tsx
+++ 4pb_gr1/client-side/cw3-react/src/App.tsx
@@ -2,11 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
-  
+  const users = [
+    "John Doe",
+    "Jane Smith",
+    "Alice Johnson",
+    "Bob Brown",
+    "Charlie Davis",
+    "Diana Evans",
+  ];
 
   return (
     <>
      <h1>Lista użytkowników</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}  
+      </ul> 
     </>
   )
 }
