cda6d1eb6d98b9dc51f262d4627c6f300a9b3a8c
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 14:02:40 +0100

    kk

--- 4pb_gr1/client-side/cw3-react/src/components/UserList.tsx
+++ 4pb_gr1/client-side/cw3-react/src/components/UserList.tsx
@@ -3,12 +3,18 @@ import User from "./User"
 
 
 const UserList = () => {
+    const elem = users.map((user, index) => (
+        <User key={index} user={user} />
+    ))
+    console.log(elem);
+
     return (
         <>
             <h1>UserList</h1>
-            {users.map((user, index) => (
+            {/* {users.map((user, index) => (
                 <User key={index} user={user} />
-            ))}
+            ))} */}
+            {elem}
         </>
     )
 }
