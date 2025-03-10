9ecfd153aa7f67129131fc17107a3907bd48c17a
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-10 17:56:20 +0100

    jj

--- 4pb_gr2/client-side/cw2-react/src/components/ManualTimer.tsx
+++ 4pb_gr2/client-side/cw2-react/src/components/ManualTimer.tsx
@@ -18,7 +18,7 @@ const ManualTimer = () => {
 
 export default ManualTimer;
 
-//  zdefiniuj komponent TextChanger, który będzie przepisywał tekst z inputa
-//  do diva podczas zmiany wartości inputa
+// zdefiniuj komponent TextChanger, który będzie przepisywał tekst z inputa
+// do diva podczas zmiany wartości inputa
 // jeśli tekst w inpucie jest dłuższy niż 20 znaków zmienia sie
 // kolor tekstu na czerwony w divie
