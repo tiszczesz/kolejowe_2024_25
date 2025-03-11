48c0ce9026609c058241bf7ee51647d473d7a740
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-11 09:45:16 +0100

    kk

--- 4pa_gr1/web-client/cw10-react/src/components/TextChanger.tsx
+++ 4pa_gr1/web-client/cw10-react/src/components/TextChanger.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react"
+import { useState } from "react";
+import { colors } from "../data/colors";
 
 const TextChanger = () => {
     const [text, setText] = useState('')
   return (
-    <div className="container">        
+    <div className="container">
+        <button>Wylosuj kolor</button>        
         <input 
         onChange={(e)=>setText(e.target.value)}
         type="text" 
         value={text} /><span>długość tekstu: {text.length}</span>
-        <div>{text}</div>
+        <div style={{fontSize:"3em"}}>{text}</div>
     </div>
   )
 }
