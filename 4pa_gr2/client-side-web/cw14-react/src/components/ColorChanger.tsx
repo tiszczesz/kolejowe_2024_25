033e45a3ce821d1dd6fbb48696f329f41b64997f
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-11 14:07:53 +0100

    kk

---
+++ 4pa_gr2/client-side-web/cw14-react/src/components/ColorChanger.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+const ColorChanger = () => {
+  return (
+    <div>
+        <select>
+
+        </select>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default ColorChanger
---
+++ 4pa_gr2/client-side-web/cw14-react/src/data/colors.ts
@@ -0,0 +1,26 @@
+export type Color = {  
+    name: string
+    hex: string
+}
+export const colors:Color[] = [
+    { name: "czerwony", hex: "#FF0000" },
+    { name: "zielony", hex: "#00FF00" },
+    { name: "niebieski", hex: "#0000FF" },
+    { name: "żółty", hex: "#FFFF00" },
+    { name: "czarny", hex: "#000000" },
+    { name: "biały", hex: "#FFFFFF" },
+    { name: "pomarańczowy", hex: "#FFA500" },
+    { name: "różowy", hex: "#FFC0CB" },
+    { name: "fioletowy", hex: "#800080" },
+    { name: "brązowy", hex: "#A52A2A" },
+    { name: "szary", hex: "#808080" },
+    { name: "granatowy", hex: "#000080" },
+    { name: "turkusowy", hex: "#40E0D0" },
+    { name: "złoty", hex: "#FFD700" },
+    { name: "srebrny", hex: "#C0C0C0" },
+    { name: "beżowy", hex: "#F5F5DC" },
+    { name: "bordowy", hex: "#800000" },
+    { name: "oliwkowy", hex: "#808000" },
+    { name: "lawendowy", hex: "#E6E6FA" },
+    { name: "łososiowy", hex: "#FA8072" }
+]
