2fc83800d7fb5d1c2fde2cf0a06e90124315f251
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-11 09:26:43 +0100

    kk

---
+++ 4pa_gr1/web-client/cw10-react/src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Ćwiczenie z użyciem stanu komponentu</h1>
+    </>
+  )
+}
+
+export default App
