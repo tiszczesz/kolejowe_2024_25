31f5d4c76095255ebd24fc7c65cf01d087e37da8
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-11 12:49:42 +0100

    kk

--- 4pa_gr2/client-side-web/cw13-react/src/components/BorderChanger.tsx
+++ 4pa_gr2/client-side-web/cw13-react/src/components/BorderChanger.tsx
@@ -1,12 +1,18 @@
 //po kliknięciu ustawia lub usuwa ramkę z elementu div.
 
+import { useState } from "react";
+
 const BorderChanger = () => {
+    const [border, setBorder] = useState(false);
   return (
-    <div style={{
+    <div
+    onClick={()=>setBorder(!border)}
+     style={{
         padding:"10px",
         backgroundColor:"lightblue",
         cursor:"pointer",
         margin:"20px",
+        border: border ? "2px solid red" : "none"
     }}>BorderChanger</div>
   )
 }
