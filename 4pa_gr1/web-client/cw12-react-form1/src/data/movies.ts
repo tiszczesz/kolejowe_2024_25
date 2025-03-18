f4a6b12200fd6ca8f140fb71bccee7d7f7a8fde6
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-03-18 09:20:37 +0100

    hh

---
+++ 4pa_gr1/web-client/cw12-react-form1/src/data/movies.ts
@@ -0,0 +1,68 @@
+export type Movie = {
+    id: number;
+    title: string;
+    category: string;
+    year: number;
+};
+export const movies: Movie[] = [
+    {
+        id: 1,
+        title: "The Shawshank Redemption",
+        category: "Drama",
+        year: 1994
+    },
+    {
+        id: 2,
+        title: "The Godfather",
+        category: "Crime",
+        year: 1972
+    },
+    {
+        id: 3,
+        title: "The Dark Knight",
+        category: "Action",
+        year: 2008
+    },
+    {
+        id: 4,
+        title: "Pulp Fiction",
+        category: "Crime",
+        year: 1994
+    },
+    {
+        id: 5,
+        title: "Forrest Gump",
+        category: "Drama",
+        year: 1994
+    },
+    {
+        id: 6,
+        title: "Inception",
+        category: "Sci-Fi",
+        year: 2010
+    },
+    {
+        id: 7,
+        title: "Fight Club",
+        category: "Drama",
+        year: 1999
+    },
+    {
+        id: 8,
+        title: "The Matrix",
+        category: "Sci-Fi",
+        year: 1999
+    },
+    {
+        id: 9,
+        title: "The Lord of the Rings: The Return of the King",
+        category: "Fantasy",
+        year: 2003
+    },
+    {
+        id: 10,
+        title: "Star Wars: Episode V - The Empire Strikes Back",
+        category: "Sci-Fi",
+        year: 1980
+    }
+];
