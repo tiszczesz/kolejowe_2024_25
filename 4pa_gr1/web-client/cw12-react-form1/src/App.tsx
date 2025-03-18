59f7044eece6fc64a4b0790ffaca8fad06bf1f53
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-03-18 09:24:23 +0100

    hh

--- 4pa_gr1/web-client/cw12-react-form1/src/App.tsx
+++ 4pa_gr1/web-client/cw12-react-form1/src/App.tsx
@@ -1,13 +1,15 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import MoviesList from './components/MoviesList'
 
 function App() {
 
 
   return (
-    <>
+    <div className='container'>
+     <MoviesList />
       
-    </>
+    </div>
   )
 }
 
---
+++ 4pa_gr1/web-client/cw12-react-form1/src/components/MoviesList.tsx
@@ -0,0 +1,11 @@
+import { movies,type Movie } from "../data/movies"
+
+
+
+function MoviesList() {
+  return (
+    <div>MoviesList</div>
+  )
+}
+
+export default MoviesList
