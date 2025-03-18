2ac4bec9e5f720a3725d6fc6cb7359c4e8c804b1
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-03-18 11:20:48 +0100

    kk

--- 4pa_gr2/client-side-web/cw14-react/src/components/ColorChanger.tsx
+++ 4pa_gr2/client-side-web/cw14-react/src/components/ColorChanger.tsx
@@ -1,20 +1,27 @@
-import {type Color,colors} from '../data/colors'
-
-
+import { useState } from "react";
+import { type Color, colors } from "../data/colors";
 
 const ColorChanger = () => {
+  const [color, setColor] = useState<string>("white");
+  function handleChange(value: string): void {
+    setColor(value);
+  }
+
   return (
     <div>
-        <select className='form-select'>
-            {colors.map((color:Color)=>(
-                <option key={color.name} value={color.hex}>{color.name}</option>
-            ))}
-        </select>
-        <div className='scene'>
-
-        </div>
+      <select
+        onChange={(e) => handleChange(e.target.value)}
+        className="form-select"
+      >
+        {colors.map((color: Color) => (
+          <option key={color.name} value={color.hex}>
+            {color.name}
+          </option>
+        ))}
+      </select>
+      <div className="scene" style={{ backgroundColor: color }}></div>
     </div>
-  )
-}
+  );
+};
 
-export default ColorChanger
+export default ColorChanger;
