8bc624f86a7dcdd1962967e4f57ff18e9b74dea4
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-03-18 11:44:54 +0100

    kk

---
+++ 4pa_gr2/client-side-web/cw15-react/src/data/games.ts
@@ -0,0 +1,79 @@
+export type Game = {
+    id: number;
+    title: string;
+    genre: string;
+    price: number;
+    year: number;
+};
+export const games: Game[] = [
+    {
+        id: 1,
+        title: "The Witcher 3: Wild Hunt",
+        genre: "RPG",
+        price: 29.99,
+        year: 2015
+    },
+    {
+        id: 2,
+        title: "Cyberpunk 2077",
+        genre: "RPG",
+        price: 59.99,
+        year: 2020
+    },
+    {
+        id: 3,
+        title: "Minecraft",
+        genre: "Sandbox",
+        price: 26.95,
+        year: 2011
+    },
+    {
+        id: 4,
+        title: "Grand Theft Auto V",
+        genre: "Action",
+        price: 29.99,
+        year: 2013
+    },
+    {
+        id: 5,
+        title: "Fortnite",
+        genre: "Battle Royale",
+        price: 0,
+        year: 2017
+    },
+    {
+        id: 6,
+        title: "Among Us",
+        genre: "Party",
+        price: 4.99,
+        year: 2018
+    },
+    {
+        id: 7,
+        title: "The Legend of Zelda: Breath of the Wild",
+        genre: "Action-adventure",
+        price: 59.99,
+        year: 2017
+    },
+    {
+        id: 8,
+        title: "Red Dead Redemption 2",
+        genre: "Action-adventure",
+        price: 59.99,
+        year: 2018
+    },
+    {
+        id: 9,
+        title: "Overwatch",
+        genre: "First-person shooter",
+        price: 39.99,
+        year: 2016
+    },
+    {
+        id: 10,
+        title: "Stardew Valley",
+        genre: "Simulation",
+        price: 14.99,
+        year: 2016
+    }
+];
