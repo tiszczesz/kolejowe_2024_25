f956c75a8b0eea3b07b5ae2ec760e77d31feb7a5
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-03-18 08:05:44 +0100

    kk

--- 4pa_gr1/web-client/cw11-react/src/App.tsx
+++ 4pa_gr1/web-client/cw11-react/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css'
-
+import {images} from './data/images'
 function App() {
 
 
   return (
     <>
-      <h1>Przeglądarka choinek</h1>
+      <h1 style={{
+        textAlign:"center",
+        borderBottom:"1px black solid"}}>Przeglądarka choinek</h1>
+      <div className='row' style={{textAlign: 'center'}}>
+        <input 
+        
+        type="button" value="&lt;&lt;" />
+        <img 
+        src={images[0]} 
+        alt={images[0]} />
+        <input type="button" value="&gt;&gt;" />
+      </div>
     </>
   )
 }
