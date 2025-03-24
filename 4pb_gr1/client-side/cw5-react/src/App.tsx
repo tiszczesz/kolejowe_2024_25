1cec73c38e967b1e9cb67e9e425f2ed1f8240117
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-24 13:10:08 +0100

    kk

--- 4pb_gr1/client-side/cw5-react/src/App.tsx
+++ 4pb_gr1/client-side/cw5-react/src/App.tsx
@@ -2,11 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 import ContacsList from './components/ContacsList'
+import SelectColor from './components/SelectColor'
 
 function App() {
   return (
     <>
       <ContacsList />
+      <hr />
+      <SelectColor />
     </>
   )
 }
---
+++ 4pb_gr1/client-side/cw5-react/src/components/SelectColor.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { colors } from "../data/colors";
+
+
+const SelectColor = () => {
+    const [listColors, setListColors] = useState<string[]>(colors);
+    const [selectedColor, setSelectedColor] = useState<string>(colors[0]);
+  return (
+    <div>
+        <div>
+            <input type="text" placeholder="dodaj kolor '#213322'" />
+            <select >
+                {listColors.map((color,index)=>(
+                    <option key={index} value={color}>{color}</option>
+                ))}
+            </select>
+        </div>
+        <div style={{
+            width: '200px', height: '100px', 
+            backgroundColor: selectedColor,
+            border: '1px solid #000',
+            }}>
+
+        </div>
+    </div>
+  )
+}
+export default SelectColor;
