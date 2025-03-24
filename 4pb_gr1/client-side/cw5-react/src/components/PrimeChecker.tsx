2981409b8a66dc6eb0f97cffb3e33a7e6a66f7f7
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-24 14:21:23 +0100

    kk

--- 4pb_gr1/client-side/cw5-react/src/components/PrimeChecker.tsx
+++ 4pb_gr1/client-side/cw5-react/src/components/PrimeChecker.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import isPrime from '../data/isPrime';
 
 
 const PrimeChecker = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
     const [info, setInfo] = useState('');
-    const [color, setColor] = useState('white');
+    // const [color, setColor] = useState('white');
   return (
     <div style={{
         padding: '20px',
         border: '1px solid #000',        
         margin: '20px auto'
         }}>
-        <input type="number" placeholder="podaj liczbę" />
+        <input ref={inputRef} type="number" placeholder="podaj liczbę" />
         <button>Sprawdź czy to liczba pierwsza</button>
         {info}
     </div>
