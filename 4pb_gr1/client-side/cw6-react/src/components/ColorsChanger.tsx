47154b564f6822945c712af0a1aa160388bb0ebf
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-24 16:27:22 +0100

    kk

--- 4pb_gr1/client-side/cw6-react/src/components/ColorsChanger.tsx
+++ 4pb_gr1/client-side/cw6-react/src/components/ColorsChanger.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import ColorRange from "./ColorRange";
+type ColorState = {
+    red: number;
+    green: number;
+    blue: number;
+}
 
 function ColorsChanger() {
-  const [colors, setColors] = useState<string[]>();
-  function onChangeColor(color: string, value: number): void {
-    console.log(`rgb(${color === "red" ? value : 0},
-        ${color === "green" ? value : 0},
-        ${color === "blue" ? value : 0})`);
-    setColors([])
+  const [colors, setColors] = useState<ColorState>({red: 0, green: 0, blue: 0});
+  function onChangeColor(color: string, value: number): void {  
+    setColors({...colors, [color]: value});
   }
 
   return (
@@ -16,6 +18,9 @@ function ColorsChanger() {
       <ColorRange colorName="red" handleChange={onChangeColor} />
       <ColorRange colorName="green" handleChange={onChangeColor} />
       <ColorRange colorName="blue" handleChange={onChangeColor} />
+      <div>
+        {colors.red}, {colors.green}, {colors.blue}
+      </div>
     </>
   );
 }
