8e82a34e9d46370379baa80aedbf95f321a62544
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-03-25 15:15:40 -0100

    kk

---
+++ 3pr_gr2/arkusze/ark2/ark2_sol/Controllers/FootballController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ark2_sol.Controllers
+{
+    public class FootballController : Controller
+    {
+        // GET: FootballController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
