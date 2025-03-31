8729dfb89e4dbd35cacdd382393e05f5951a4afd
tiszczesz <tiszczesz@users.noreply.github.com>
2025-03-31 14:21:51 +0200

    kk

--- 4pb_gr1/client-side/zad_react1/zad1-movies/src/main.tsx
+++ 4pb_gr1/client-side/zad_react1/zad1-movies/src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+console.log("Hello world");
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
     <App />
   </StrictMode>,
