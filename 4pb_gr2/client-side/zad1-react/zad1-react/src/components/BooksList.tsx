0ccd3241e32111b1812a6e1f331d664f90faee16
tiszczesz <tiszczesz@users.noreply.github.com>
2025-04-07 19:25:04 +0200

    kk

--- 4pb_gr2/client-side/zad1-react/zad1-react/src/components/BooksList.tsx
+++ 4pb_gr2/client-side/zad1-react/zad1-react/src/components/BooksList.tsx
@@ -2,39 +2,49 @@ import { ComponentProps, useState } from "react";
 import { Book, books, genres } from "../data/books-data";
 import BookComponent from "./BookComponent";
 type Props = {
-    handleAdd: (book:Book)=> void;
+  handleAdd: (book: Book) => void;
 } & ComponentProps<"div">;
-const BooksList = (props:Props) => {
+const BooksList = (props: Props) => {
   //const [booksList, setBooksList] = useState(books);
   const [selectedGenre, setSelectedGenre] = useState("all");
   return (
-    <div style={{}} >
+    <div style={{}}>
       <h2 className="header">Lista książek</h2>
       <div className="row m-2">
         <label htmlFor="selectedGenre" className="col-3 text-end">
           Wybierz gatunek:
         </label>
-        <select className="col-6" name="selectedGenre" 
-        onChange={(e)=>setSelectedGenre(e.target.value)}
-        id="selectedGenre">
-            <option value="all">all</option>
-            {genres.map((genre) => (
-              <option key={genre} value={genre}>
-                {genre}
-              </option>
-            ))}
+        <select
+          className="col-6"
+          name="selectedGenre"
+          onChange={(e) => setSelectedGenre(e.target.value)}
+          id="selectedGenre"
+        >
+          <option value="all">all</option>
+          {genres.map((genre) => (
+            <option key={genre} value={genre}>
+              {genre}
+            </option>
+          ))}
         </select>
       </div>
       <section
         className="d-flex flex-wrap justify-content-center"
         style={{
-          gap: "10px"          
+          gap: "10px",
+          minWidth: "1000px",
         }}
       >
-        {books.map((book) => (
-           (selectedGenre===book.genre || selectedGenre==='all') && 
-           <BookComponent key={book.id} book={book} addToBasket={props.handleAdd} />
-        ))}
+        {books.map(
+          (book) =>
+            (selectedGenre === book.genre || selectedGenre === "all") && (
+              <BookComponent
+                key={book.id}
+                book={book}
+                addToBasket={props.handleAdd}
+              />
+            )
+        )}
       </section>
     </div>
   );
