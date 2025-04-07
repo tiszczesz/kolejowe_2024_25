2d5f8f8d40112de7b0a3d50bf79130e436380616
tiszczesz <tiszczesz@users.noreply.github.com>
2025-04-07 14:08:20 +0200

    kk

---
+++ 4pb_gr1/client-side/cw7-react-forms/src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='container'>
+      <h1>Formularze</h1>
+    </div>
+  )
+}
+
+export default App
