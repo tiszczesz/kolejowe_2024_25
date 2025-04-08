19851c9165c15bb360f2bdb4c979a15fe5727df2
tiszczesz <tiszczesz@users.noreply.github.com>
2025-04-08 07:56:46 +0200

    kk

---
+++ 4pa_gr1/web-client/cw16-react-zad1/instrukcja_zadanie.md
@@ -0,0 +1,6 @@
+# Zadanie 
+Utwórz aplikację React która:
+1. Wyświetla listę wszystkich produktów
+2. umozliwia dodawanie nowych produktów poprzez formularz
+3. umożliwia usuwanie wybranego produktu
+4. Dla ambitnych dodaj możliwość filtrowania po kategoriach
---
+++ 4pa_gr1/web-client/cw16-react-zad1/src/data/products.ts
@@ -0,0 +1,91 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+};
+export const Categories = [
+    'Electronics',
+    'Books',
+    'Clothing',
+    'Home & Kitchen',
+    'Sports & Outdoors',
+    'Toys & Games',
+    'Beauty & Personal Care',
+    'Automotive',
+    'Health & Household',
+    'Grocery & Gourmet Food'
+];
+export const Products: Product[] = [
+    {
+        id: 1,
+        name: 'Wireless Headphones',
+        description: 'High-quality wireless headphones with noise cancellation.',
+        price: 99.99,
+        category: 'Electronics'
+    },
+    {
+        id: 2,
+        name: 'Fantasy Novel',
+        description: 'A captivating fantasy novel with an epic storyline.',
+        price: 14.99,
+        category: 'Books'
+    },
+    {
+        id: 3,
+        name: 'Running Shoes',
+        description: 'Comfortable and durable running shoes for all terrains.',
+        price: 59.99,
+        category: 'Sports & Outdoors'
+    },
+    {
+        id: 4,
+        name: 'Blender',
+        description: 'A powerful blender for smoothies and food preparation.',
+        price: 49.99,
+        category: 'Home & Kitchen'
+    },
+    {
+        id: 5,
+        name: 'Action Figure',
+        description: 'A collectible action figure from a popular franchise.',
+        price: 19.99,
+        category: 'Toys & Games'
+    },
+    {
+        id: 6,
+        name: 'Lipstick Set',
+        description: 'A set of vibrant lipsticks in various shades.',
+        price: 24.99,
+        category: 'Beauty & Personal Care'
+    },
+    {
+        id: 7,
+        name: 'Car Vacuum Cleaner',
+        description: 'A compact vacuum cleaner designed for car interiors.',
+        price: 29.99,
+        category: 'Automotive'
+    },
+    {
+        id: 8,
+        name: 'Vitamin C Tablets',
+        description: 'A bottle of Vitamin C tablets to boost immunity.',
+        price: 12.99,
+        category: 'Health & Household'
+    },
+    {
+        id: 9,
+        name: 'Organic Coffee Beans',
+        description: 'Premium organic coffee beans for a rich flavor.',
+        price: 15.99,
+        category: 'Grocery & Gourmet Food'
+    },
+    {
+        id: 10,
+        name: 'Winter Jacket',
+        description: 'A warm and stylish winter jacket for cold weather.',
+        price: 89.99,
+        category: 'Clothing'
+    }
+]
