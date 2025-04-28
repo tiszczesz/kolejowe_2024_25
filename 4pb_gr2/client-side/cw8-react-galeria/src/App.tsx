87e582ef0588a717a2ea4ad65485f129aafa5153
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-04-28 18:55:53 +0200

    kk

--- 4pb_gr2/client-side/cw8-react-galeria/src/App.tsx
+++ 4pb_gr2/client-side/cw8-react-galeria/src/App.tsx
@@ -1,13 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import CarViewer from './components/CarViewer';
 
 function App() {
  
 
   return (
-    <>
-    
-    </>
+    <div className='container text-center'>
+     <h1>Przeglądarka samochodów</h1>
+     <CarViewer />
+    </div>
   )
 }
 
