a9679af25657b917ddd2c56a0170ecabb05d3ace
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-04-28 17:23:21 +0200

    kk

--- 4pb_gr2/client-side/cw7-react/src/App.tsx
+++ 4pb_gr2/client-side/cw7-react/src/App.tsx
@@ -1,12 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProductComp from "./components/ProductComp";
+import motyl1 from "/images/motyl1.jpg";
 
 function App() {
   return (
     <div className="container">
       <h1>Praca z obiektami i stanem</h1>
       <ProductComp />
+      <hr />
+      <div>
+        <img src={motyl1} alt={motyl1} />
+      </div>
     </div>
   );
 }
--- 4pb_gr2/client-side/cw7-react/src/components/ProductComp.tsx
+++ 4pb_gr2/client-side/cw7-react/src/components/ProductComp.tsx
@@ -1,34 +1,72 @@
 import { useState } from "react";
 
 type Product = {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    image: string;
-}
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+};
 
 const ProductComp = () => {
-    const [product, setProduct] = useState<Product | null>({
-        id: 1,
-        name: "Motylek 1",
-        price: 100,
-        description: "This is product 1",
-        image: "motyl1.jpg"
-    });
+  const [product, setProduct] = useState<Product | null>({
+    id: 1,
+    name: "Motylek 1",
+    price: 100,
+    description: "This is product 1",
+    image: "motyl1.jpg",
+  });
+  function handleImageChange(): void {
+    if (product) {
+      const newProduct: Product = {
+        ...product,
+        image: product.image === "motyl1.jpg" ? "motyl2.jpg" : "motyl1.jpg",
+      };
+      setProduct(newProduct);
+    }
+  }
+  function handleNameChange(): void {
+    if(product){
+        const newProduct: Product = {
+            ...product,
+            name: product.name = product.name+"X",
+        };
+        setProduct(newProduct);
+    }
+  }
+  function handlePriceChange(): void {
+    if(product){
+        const newProduct: Product = {
+            ...product,
+            price: product.price*2,
+        };
+        setProduct(newProduct);
+    }
+  }
+
   return (
     <div>
-        <h3>{product?.name}</h3>
-        <img src={'images/'+product?.image} 
-        alt={product?.name} 
-        />
-        <p>{product?.description}</p>   
-        <p>Price: {product?.price} PLN</p>
-        <button className="btn btn-secondary m-2">Zmień obrazek </button><br/>
-        <button className="btn btn-secondary m-2">Zmień nazwę</button><br/>
-        <button className="btn btn-secondary m-2">Zmień cenę</button><br/>
+      <h3>{product?.name}</h3>
+      <img src={"images/" + product?.image} alt={product?.name} />
+      <p>{product?.description}</p>
+      <p>Price: {product?.price} PLN</p>
+      <button
+        onClick={() => handleImageChange()}
+        className="btn btn-secondary m-2"
+      >
+        Zmień obrazek{" "}
+      </button>
+      <br />
+      <button
+      onClick={()=>handleNameChange()}
+       className="btn btn-secondary m-2">Zmień nazwę</button>
+      <br />
+      <button
+      onClick={()=>handlePriceChange()}
+       className="btn btn-secondary m-2">Zmień cenę</button>
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default ProductComp
+export default ProductComp;
