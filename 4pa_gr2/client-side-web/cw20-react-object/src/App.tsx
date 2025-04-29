c3c080fbd3200fd6f5c35b25c2946195e1d4edbf
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-04-29 12:35:39 +0200

    kk

--- 4pa_gr2/client-side-web/cw20-react-object/src/App.tsx
+++ 4pa_gr2/client-side-web/cw20-react-object/src/App.tsx
@@ -1,36 +1,75 @@
-import { useState } from 'react'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
 type Movie = {
-  id: number,
-  title: string,
-  year: number,
-  rating: number,
-  image: string,
-}
-
+  id: number;
+  title: string;
+  year: number;
+  rating: number;
+  image: string;
+};
 
 function App() {
- 
   //tworzymy obiekt opisujacy stan
   const [movie, setMovie] = useState<Movie>({
     id: 1,
-    title: 'Górnicy w Kosmosie',
+    title: "Górnicy w Kosmosie",
     year: 2021,
     rating: 3,
-    image: 'images/gornicy.jpg'
-  })
+    image: "images/gornicy.jpg",
+  });
+  function handleImageClick(): void {
+    //zmiana stanu obiektu movie
+    const newMovie: Movie = {
+      ...movie,
+      image:
+        movie.image === "images/gornicy.jpg"
+          ? "images/gornicy2.jpg"
+          : "images/gornicy.jpg",
+    };
+    //zmiana stanu komponentu --> renderowanie komponentu
+    setMovie(newMovie);
+  }
+
+  function handleClick(): void {
+    const newMovie: Movie = {
+      ...movie,
+      rating: movie.rating + 1,
+    };
+    setMovie(newMovie);
+  }
+
   return (
     <>
       <h1>Zankomite filmy</h1>
-      <div>
+      <div
+        style={{
+          border: "1px solid black",
+          padding: "10px",
+          margin: "10px",
+          backgroundColor: movie.rating > 20 ? "green" : "white",
+        }}
+      >
         <h3>{movie.title}</h3>
         <h5>Rok produkcji: {movie.year}</h5>
         <div>
-          <img src={movie.image} alt={movie.image} />
+          <img style={{
+            cursor: "pointer",
+          }}
+            onClick={() => handleImageClick()}
+            src={movie.image}
+            alt={movie.image}
+          />
+        </div>
+        <div className="m-3">
+          <span>ilość polubień: {movie.rating}</span>
+          <button
+          onClick={()=>handleClick()}
+           className="btn btn-primary m-1">Polub</button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
