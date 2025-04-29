c5e5011a11e7d1d824b2e4168a6c10dbb2aab35b
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-04-29 08:01:45 +0200

    kk

---
+++ 4pa_gr1/web-client/cw18-react-objects/src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='container'>
+      <h1>Stan komponentu z obiektem</h1>
+    </div>
+  )
+}
+
+export default App
