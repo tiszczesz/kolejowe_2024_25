b352cd845c79aeca5b794482f87ba5cad5b7a6fb
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-05-12 15:16:45 +0200

    first

---
+++ 4pb_gr1/client-side/ark2/src/App.tsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='container'>
+     
+    </div>
+  )
+}
+
+export default App
