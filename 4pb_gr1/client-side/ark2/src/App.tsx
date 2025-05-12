cf910dc6051a03472e6f2a0c1590d3be2c180653
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-05-12 19:20:25 +0200

    kk

--- 4pb_gr1/client-side/ark2/src/App.tsx
+++ 4pb_gr1/client-side/ark2/src/App.tsx
@@ -3,14 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import FormComp from './components/FormComp';
 import FormCompWithState from './components/FormCompWithState';
+import FormWithAction from './components/FormWithAction';
 
 function App() {
  
 
   return (
     <div className='d-flex'>
-      <div className='w-50'><FormComp /></div>
-     <div className='w-50'><FormCompWithState /></div>
+      <div style={{width:"30%"}} ><FormComp /></div>
+     <div style={{width:"30%"}} ><FormCompWithState /></div>
+      <div style={{width:"30%"}} ><FormWithAction /></div>
      
     </div>
   )
