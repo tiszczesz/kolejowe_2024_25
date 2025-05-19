d8da470d18a77e56cb5b47ff81f44a0c4b47fd7e
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-05-19 15:55:12 +0200

    kk

--- 4pb_gr1/client-side/ark3-images/src/App.tsx
+++ 4pb_gr1/client-side/ark3-images/src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { useState } from "react";
+import { items, type Item } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [flowers, setFlowers] = useState(true);
+  const [animals, setAnimals] = useState(true);
+  const [cars, setCars] = useState(true);
+  const [images, setImages] = useState<Item[]>(items);
+  function handleClick(id: number): void {
+    const newImages = images.map((elem) => {
+      if (elem.id === id) {
+        return { ...elem, downloads: elem.downloads + 1 };
+      }
+      return elem;
+    }
+    );
+    setImages(newImages);
+    console.log(newImages);    
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>Kategorie zdjęć</h1>
+      <section style={{ display: "flex", gap: "10px" }}>
+        <span className="form-switch">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            checked={flowers}
+            id="flowersId"
+          />
+          <label className="form-check-label" htmlFor="flowersId">
+            Kwiaty
+          </label>
+        </span>
+        <span className="form-switch">
+          <input
+            className="form-check-input"
+            checked={animals}
+            type="checkbox"
+            id="animalsId"
+          />
+          <label className="form-check-label" htmlFor="animalsId">
+            Zwierzęta
+          </label>
+        </span>
+        <span className="form-switch">
+          <input
+            className="form-check-input"
+            checked={cars}
+            type="checkbox"
+            id="carsId"
+          />
+          <label className="form-check-label" htmlFor="carsId">
+            Samochody
+          </label>
+        </span>
+      </section>
+      <section style={{ display: "flex", 
+        flexWrap:"wrap",
+        gap: "10px" }}>
+        {images.map((elem, index) => (
+          <div key={index}>
+            <img src={"assets/" + elem.filename} alt={elem.alt} />
+            <div>Pobrań: {elem.downloads}</div>
+            <button onClick={()=>handleClick(elem.id)} className="btn btn-success">Pobierz</button>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
