068f8d201bd2a4852b73dcefbbffb7c1680b954f
tiszczesz <tiszczesz@users.noreply.github.com>
2025-06-09 14:37:37 +0200

    kkk

--- 4pb_gr1/client-side/cw10-react-useeffect/src/App.tsx
+++ 4pb_gr1/client-side/cw10-react-useeffect/src/App.tsx
@@ -1,11 +1,18 @@
 
 import { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
   console.log("render App");
 
-  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [colorA, setColorA] = useState("rgb(123,23,123)");
+  const [colorB, setColorB] = useState("rgb(123,23,123)");
+  const [colorC, setColorC] = useState("rgb(123,23,123)");
+  const [colorD, setColorD] = useState("rgb(123,23,123)");
+  const [colorE, setColorE] = useState("rgb(123,23,123)");
+
   useEffect(() => {
     console.log("usEffect");
     const interval = setInterval(() => {
@@ -19,6 +26,15 @@ function App() {
       <div>
         Aktualny czas: {time}
       </div>
+      <div className='d-flex gap-2 m-3'>
+        <div className="color" style={{backgroundColor:colorA}}></div>
+        <div className="color" style={{backgroundColor:colorB}}></div>
+        <div className="color" style={{backgroundColor:colorB}}></div>
+        <div className="color" style={{backgroundColor:colorB}}></div>
+        <div className="color" style={{backgroundColor:colorB}}></div>
+      </div>
+
+
     </>
   )
 }
