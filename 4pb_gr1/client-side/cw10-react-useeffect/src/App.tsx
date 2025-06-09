221225c61c22f7d72a2e65b4e99b436246c16030
tiszczesz <tiszczesz@users.noreply.github.com>
2025-06-09 14:23:28 +0200

    kk

--- 4pb_gr1/client-side/cw10-react-useeffect/src/App.tsx
+++ 4pb_gr1/client-side/cw10-react-useeffect/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("render App");
 
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  useEffect(() => {
+    console.log("usEffect");
+    const interval = setInterval(() => {
+      console.log("w setInterval");
+      setTime(new Date().toLocaleTimeString())
+      return () => clearInterval(interval);
+    }, 1000)
+  }, []);
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Aktualny czas: {time}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
