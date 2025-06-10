d588cf46c0a3b89b65e462416ceee54682d7efe3
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-06-10 12:55:29 +0200

    kk

---
+++ 4pa_gr2/last/mywebApp/Models/Person.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mywebApp.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
